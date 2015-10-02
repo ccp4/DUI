87ad8129ba1878e1f5bcb8332950f9f1d9b42a88
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2015-10-02 15:56:17 +0100

    playing with boxes and DUI

--- first_pyside_prototipe/shell_n_box.py
+++ first_pyside_prototipe/shell_n_box.py
@@ -1,9 +1,7 @@
 from subprocess import call as shell_func
 import sys
-#from PySide import QtGui
 
 from PyQt4 import QtGui, QtCore
-
 
 class Example(QtGui.QWidget):
 
@@ -13,30 +11,36 @@ class Example(QtGui.QWidget):
 
     def initUI(self):
 
-        hbox = QtGui.QVBoxLayout(self)
-
-        self.btn1 = QtGui.QPushButton('    import    ', self)
-        hbox.addWidget(self.btn1)
+        self.btn1 = QtGui.QPushButton('\n\n     import    \n\n', self)
         self.btn1.clicked.connect(self.B_clicked1)
 
-        self.btn2 = QtGui.QPushButton(' find_spots ', self)
-        hbox.addWidget(self.btn2)
+        self.btn2 = QtGui.QPushButton('\n\n find_spots \n\n', self)
         self.btn2.clicked.connect(self.B_clicked2)
 
-        self.btn3 = QtGui.QPushButton('    index     ', self)
-        hbox.addWidget(self.btn3)
+        self.btn3 = QtGui.QPushButton('\n\n    index     \n\n', self)
         self.btn3.clicked.connect(self.B_clicked3)
 
         self.lin_txt = QtGui.QLineEdit(self)
-        hbox.addWidget(self.lin_txt)
-
-        self.btn_go = QtGui.QPushButton('      Go      ', self)
-        hbox.addWidget(self.btn_go)
+        self.btn_go = QtGui.QPushButton('\n      Go      \n', self)
         self.btn_go.clicked.connect(self.B_go_clicked)
 
-
-
-        self.setLayout(hbox)
+        print "Hi 01"
+        vbox = QtGui.QVBoxLayout()
+        vbox.addWidget(self.btn1)
+        vbox.addWidget(self.btn2)
+        vbox.addWidget(self.btn3)
+        print "Hi 02"
+        hbox = QtGui.QHBoxLayout()
+        hbox.addWidget(self.lin_txt)
+        hbox.addWidget(self.btn_go)
+        print "Hi 03"
+        bg_box = QtGui.QVBoxLayout(self)
+        bg_box.addLayout(vbox)
+        bg_box.addStretch(1)
+        bg_box.addLayout(hbox)
+        print "Hi 04"
+        self.setGeometry(100, 200, 1150, 850)
+        self.setLayout(bg_box)
         self.setWindowTitle('Shell dialog')
         self.show()
 
