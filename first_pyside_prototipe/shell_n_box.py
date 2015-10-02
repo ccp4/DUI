08a3d72e4251fc2be072b0da3ed5225c6e528058
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2015-10-02 12:55:32 +0100

    playing with boxes

--- first_pyside_prototipe/shell_n_box.py
+++ first_pyside_prototipe/shell_n_box.py
@@ -1,6 +1,9 @@
 from subprocess import call as shell_func
 import sys
-from PySide import QtGui
+#from PySide import QtGui
+
+from PyQt4 import QtGui, QtCore
+
 
 class Example(QtGui.QWidget):
 
@@ -10,26 +13,33 @@ class Example(QtGui.QWidget):
 
     def initUI(self):
 
+        hbox = QtGui.QVBoxLayout(self)
+
         self.btn1 = QtGui.QPushButton('    import    ', self)
-        self.btn1.move(20, 20)
+        hbox.addWidget(self.btn1)
         self.btn1.clicked.connect(self.B_clicked1)
+
         self.btn2 = QtGui.QPushButton(' find_spots ', self)
-        self.btn2.move(20, 60)
+        hbox.addWidget(self.btn2)
         self.btn2.clicked.connect(self.B_clicked2)
+
         self.btn3 = QtGui.QPushButton('    index     ', self)
-        self.btn3.move(20, 100)
+        hbox.addWidget(self.btn3)
         self.btn3.clicked.connect(self.B_clicked3)
 
+        self.lin_txt = QtGui.QLineEdit(self)
+        hbox.addWidget(self.lin_txt)
+
         self.btn_go = QtGui.QPushButton('      Go      ', self)
-        self.btn_go.move(300,180)
+        hbox.addWidget(self.btn_go)
         self.btn_go.clicked.connect(self.B_go_clicked)
 
-        self.lin_txt = QtGui.QLineEdit(self)
-        self.lin_txt.move(20, 182)
 
-        self.setGeometry(100, 200, 500, 250)
+
+        self.setLayout(hbox)
         self.setWindowTitle('Shell dialog')
         self.show()
+
 
     def B_clicked1(self):
         self.lin_txt.setText(str("dials.import"))
