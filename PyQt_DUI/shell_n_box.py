83f81bc1e0bb2c2ba42adef06a8d6b1ab43d6c44
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2015-10-06 17:02:50 +0100

    complet CLI with some defaults

--- PyQt_DUI/shell_n_box.py
+++ PyQt_DUI/shell_n_box.py
@@ -9,41 +9,42 @@ from PyQt4.QtCore import *
 
 class step(object):
     def __init__(self):
-        self.Bttlabel = "\n\n     import    \n\n"
-        self.action = "dials.import"
+        self.Bttlabel = "\n\n     CLI command    \n\n"
+        self.action = "dials.some_command"
 
 
 steps = []
 
 import_step = step()
-#import_step.Bttlabel = "\n\n     import    \n\n"
-#import_step.action = "dials.import"
+import_step.Bttlabel = "\n\n     import    \n\n"
+import_step.action = "dials.import ~/data/th_8_2_0*"
 steps.append(import_step)
 
 find_step = step()
 find_step.Bttlabel = "\n\n     find spots    \n\n"
-find_step.action = "dials.find_spots"
+find_step.action = "dials.find_spots datablock.json"
 steps.append(find_step)
 
 index_step = step()
 index_step.Bttlabel = "\n\n     index    \n\n"
-index_step.action = "dials.index"
+index_step.action = "dials.index datablock.json strong.pickle"
 steps.append(index_step)
 
 refine_step = step()
 refine_step.Bttlabel = "\n\n     refine    \n\n"
-refine_step.action = "dials.refine"
+refine_step.action = "dials.refine experiments.json indexed.pickle"
 steps.append(refine_step)
 
 integrate_step = step()
 integrate_step.Bttlabel = "\n\n     integrate    \n\n"
-integrate_step.action = "dials.integrate"
+integrate_step.action = "dials.integrate refined_experiments.json refined.pickle"
 steps.append(integrate_step)
 
 export_step = step()
 export_step.Bttlabel = "\n\n     export    \n\n"
-export_step.action = "dials.export"
+export_step.action = "dials.export integrated.pickle integrated.h5"
 steps.append(export_step)
+
 
 class Example( QWidget):
 
