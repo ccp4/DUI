2ff8747ba0a3b883831ad7231c1f64368edcf1ea
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2015-10-22 15:53:39 +0100

    building separate pieces to later plug in

--- dui/stacked_widgets.py
+++ dui/stacked_widgets.py
@@ -130,3 +130,42 @@ class ExportPage(QtGui.QWidget):
 
         self.setLayout(mainLayout)
 
+
+
+import sys
+
+
+class Example(QtGui.QWidget):
+
+    def __init__(self):
+        super(Example, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+
+        hbox = QtGui.QHBoxLayout(self)
+        pixmap = QtGui.QPixmap("dartlang_logo_small.png")
+
+        lbl = QtGui.QLabel(self)
+        lbl.setPixmap(pixmap)
+
+        hbox.addWidget(lbl)
+        self.setLayout(hbox)
+
+        self.move(300, 200)
+        self.setWindowTitle('Red Rock')
+        self.show()
+
+def main():
+
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
