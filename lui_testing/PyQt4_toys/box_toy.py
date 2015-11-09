9e841f0a6c36efa21ef4c43743b673186ce85202
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2015-11-09 17:52:37 +0000

    playing with CLI output with Popen

--- lui_testing/PyQt4_toys/box_toy.py
+++ lui_testing/PyQt4_toys/box_toy.py
@@ -1,4 +1,5 @@
-from subprocess import call as shell_func
+#from subprocess import call as shell_func
+import subprocess
 import sys
 
 from PySide import QtGui, QtCore
@@ -15,8 +16,14 @@ class Example(QtGui.QWidget):
         self.btn_go = QtGui.QPushButton('\n      Go      \n', self)
         self.btn_go.clicked.connect(self.B_go_clicked)
 
+
+        self.multi_line_txt = QtGui.QTextEdit()
+
         vbox = QtGui.QVBoxLayout()
         vbox.addWidget(self.btn1)
+        vbox.addWidget(self.multi_line_txt)
+
+
 
         top_box = QtGui.QHBoxLayout()
         top_box.addLayout(vbox)
@@ -41,9 +48,51 @@ class Example(QtGui.QWidget):
 
     def B_go_clicked(self):
         shell_str = str(self.lin_txt.text())
-        shell_func(shell_str, shell=True)
-        #p = subprocess.Popen(cmd, stdout=subprocess.PIPE, bufsize=1)
+        #shell_func(shell_str, shell=True)
+
+
+        p = subprocess.Popen(shell_str, stdout = subprocess.PIPE, bufsize = 1, shell = True)
+        for line in iter(p.stdout.readline, b''):
+            print line,
+            #self.multi_line_txt.append("Hi")
+            self.multi_line_txt.append(line)
+
+        p.stdout.close()
+        p.wait()
+
+
         self.lin_txt.setText(str(""))
+
+to_copy_from = '''
+
+class MyQProcess(QProcess):
+  def __init__(self):
+   #Call base class method
+   QProcess.__init__(self)
+   #Create an instance variable here (of type QTextEdit)
+   self.edit  = QTextEdit()
+   self.edit.setWindowTitle("QTextEdit Standard Output Redirection")
+   self.edit.show()
+
+  #Define Slot Here
+  @pyqtSlot()
+  def readStdOutput(self):
+    self.edit.append(QString(self.readAllStandardOutput()))
+
+
+def main():
+    app   = QApplication(sys.argv)
+    qProcess  = MyQProcess()
+
+    qProcess.setProcessChannelMode(QProcess.MergedChannels);
+    #qProcess.start("ldconfig -v")
+    qProcess.start("ls -al")
+
+    QObject.connect(qProcess,SIGNAL("readyReadStandardOutput()"),qProcess,SLOT("readStdOutput()"));
+
+    return app.exec_()
+
+'''
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
