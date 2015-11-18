7ab578b50eff3b7769431dfb312c2a9625f4a375
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2015-11-18 14:18:21 +0000

    adding first toy with phil scope

---
+++ lui_testing/phil_test/first_exe.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# FIXME Copied from dials.index.py. This is needed here because scipy needs to
+# be imported before cctbx otherwise there will be a segmentation fault. This
+# should be fixed in dials.index so that we don't need to import here.
+try:
+  # try importing scipy.linalg before any cctbx modules, otherwise we
+  # sometimes get a segmentation fault/core dump if it is imported after
+  # scipy.linalg is a dependency of sklearn.cluster.DBSCAN
+  import scipy.linalg # import dependency
+except ImportError, e:
+  pass
+
+
+#from dials.command_line.integrate import phil_scope
+#from dials.command_line.refine import phil_scope
+#from dials.command_line.index import phil_scope
+
+
+from dials.command_line.find_spots import phil_scope
+
+print phil_scope.objects
