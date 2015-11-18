ad08ddfd3ddab06e6f94ad5e7de34e3ea48539e2
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2015-11-18 17:28:59 +0000

    navigating recursively thru phil scope

--- lui_testing/phil_test/first_exe.py
+++ lui_testing/phil_test/first_exe.py
@@ -11,12 +11,27 @@ try:
 except ImportError, e:
   pass
 
-#from dials.command_line.integrate import phil_scope
-#from dials.command_line.refine import phil_scope
-#from dials.command_line.index import phil_scope
-from dials.command_line.find_spots import phil_scope
 
-for single_obj in phil_scope.objects:
-  print single_obj
 
-print phil_scope.objects
+def reuse(phl_obj):
+  for single_obj in phl_obj:
+    if( single_obj.is_scope ):
+      print "is_scope" # reuse here
+      reuse(single_obj.objects)
+    elif( single_obj.is_definition):
+      print "is_definition"
+
+    lst_obj.append(single_obj)
+
+
+if( __name__ == "__main__" ):
+  #from dials.command_line.integrate import phil_scope
+  #from dials.command_line.refine import phil_scope
+  #from dials.command_line.index import phil_scope
+  from dials.command_line.find_spots import phil_scope
+  phl_obj = phil_scope.objects
+  lst_obj = []
+  reuse(phl_obj)
+
+  for single_obj in lst_obj:
+    print single_obj
