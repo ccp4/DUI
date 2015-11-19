68e19cefec64359f04771286e4ee8045bae904e0
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2015-11-19 14:09:23 +0000

    refmoving a function fron inside a class

--- lui_testing/phil_test/funcs.py
+++ lui_testing/phil_test/funcs.py
@@ -1,13 +1,13 @@
+def test1(var1):
+    print "from test1"
+    print "var1 =", var1
 
 class anything(object):
 
     def call_me(self):
 
-        def test1(var1):
-            print "from test1"
-            print "var1 =", var1
         print "after def test1"
-        #test1(5)
+        test1(5)
 
 if( __name__ == "__main__" ):
     print "Hi"
