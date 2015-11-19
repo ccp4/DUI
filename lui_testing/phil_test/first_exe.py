23e0506959469f7c96789dbf368c673fecd1b599
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2015-11-19 11:31:39 +0000

    navigating recursively thru phil scope part 3

--- lui_testing/phil_test/first_exe.py
+++ lui_testing/phil_test/first_exe.py
@@ -11,25 +11,30 @@ try:
 except ImportError, e:
   pass
 
-def reuse(phl_obj):
+def deep_in_rec(phl_obj):
   for single_obj in phl_obj:
     if( single_obj.is_scope ):
-      print "is_scope" # reuse here
-      reuse(single_obj.objects)
+      print "is_scope \n" # deep_in_rec here
+      deep_in_rec(single_obj.objects)
     elif( single_obj.is_definition):
       print "single_obj.name =", single_obj.name
-      print "single_obj.extract =", single_obj.extract
+      local_val = single_obj.extract()
+
+      print "single_obj.extract =", local_val
+      print "type(local_val) =", type(local_val)
 
     lst_obj.append(single_obj)
 
 if( __name__ == "__main__" ):
-  from dials.command_line.integrate import phil_scope
+  #from dials.command_line.integrate import phil_scope
   #from dials.command_line.refine import phil_scope
   #from dials.command_line.index import phil_scope
-  #from dials.command_line.find_spots import phil_scope
+  from dials.command_line.find_spots import phil_scope
   phl_obj = phil_scope.objects
   lst_obj = []
-  reuse(phl_obj)
+  deep_in_rec(phl_obj)
 
+  '''
   for single_obj in lst_obj:
     print single_obj
+  '''
