1165ac01f668a8bbf1a97360c5b04d1241b15c63
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-01-12 12:11:36 +0000

    working on simple example of tab use

--- lui_testing/PyQt4_toys/tabdialog_simple.py
+++ lui_testing/PyQt4_toys/tabdialog_simple.py
@@ -1,97 +1,38 @@
 #!/usr/bin/env python
 
-#############################################################################
-##
-## Copyright (C) 2004-2005 Trolltech AS. All rights reserved.
-##
-## This file is part of the example classes of the Qt Toolkit.
-##
-## This file may be used under the terms of the GNU General Public
-## License version 2.0 as published by the Free Software Foundation
-## and appearing in the file LICENSE.GPL included in the packaging of
-## this file.  Please review the following information to ensure GNU
-## General Public Licensing requirements will be met:
-## http://www.trolltech.com/products/qt/opensource.html
-##
-## If you are unsure which license is appropriate for your use, please
-## review the following information:
-## http://www.trolltech.com/products/qt/licensing.html or contact the
-## sales department at sales@trolltech.com.
-##
-## This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-## WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-##
-#############################################################################
-
 from PySide import QtCore, QtGui
 
-
 class TabDialog(QtGui.QDialog):
-    def __init__(self, fileName, parent=None):
+    def __init__(self, parent=None):
         super(TabDialog, self).__init__(parent)
 
-        fileInfo = QtCore.QFileInfo(fileName)
-
         tabWidget = QtGui.QTabWidget()
-        tabWidget.addTab(GeneralTab(fileInfo), "General")
-        tabWidget.addTab(PermissionsTab(fileInfo), "Permissions")
-        tabWidget.addTab(ApplicationsTab(fileInfo), "Applications")
-
-        buttonBox = QtGui.QDialogButtonBox(QtGui.QDialogButtonBox.Ok | QtGui.QDialogButtonBox.Cancel)
-
-        buttonBox.accepted.connect(self.accept)
-        buttonBox.rejected.connect(self.reject)
+        tabWidget.addTab(PermissionsTab(), "Tab 1")
+        tabWidget.addTab(ApplicationsTab(), "tab 2")
 
         mainLayout = QtGui.QVBoxLayout()
         mainLayout.addWidget(tabWidget)
-        mainLayout.addWidget(buttonBox)
         self.setLayout(mainLayout)
-
-        self.setWindowTitle("Tab Dialog")
-
-
-class GeneralTab(QtGui.QWidget):
-    def __init__(self, fileInfo, parent=None):
-        super(GeneralTab, self).__init__(parent)
-        fileNameLabel = QtGui.QLabel("File Name:")
-        fileNameEdit = QtGui.QLineEdit(fileInfo.fileName())
-        mainLayout = QtGui.QVBoxLayout()
-        mainLayout.addWidget(fileNameLabel)
-        mainLayout.addWidget(fileNameEdit)
-        mainLayout.addStretch(1)
-        self.setLayout(mainLayout)
-
+        self.setWindowTitle("2 Tabs Dialog")
 
 class PermissionsTab(QtGui.QWidget):
-    def __init__(self, fileInfo, parent=None):
+    def __init__(self, parent=None):
         super(PermissionsTab, self).__init__(parent)
-        readable = QtGui.QCheckBox("Readable")
-        if fileInfo.isReadable():
-            readable.setChecked(True)
-        permissionsLayout = QtGui.QVBoxLayout()
-        permissionsLayout.addWidget(readable)
-        self.setLayout(permissionsLayout)
-
+        readable = QtGui.QCheckBox("CheckBox")
+        localLayout = QtGui.QVBoxLayout()
+        localLayout.addWidget(readable)
+        self.setLayout(localLayout)
 
 class ApplicationsTab(QtGui.QWidget):
-    def __init__(self, fileInfo, parent=None):
+    def __init__(self, parent=None):
         super(ApplicationsTab, self).__init__(parent)
-        topLabel = QtGui.QLabel("Open with:")
-        layout = QtGui.QVBoxLayout()
-        layout.addWidget(topLabel)
-        self.setLayout(layout)
-
+        topLabel = QtGui.QLabel("label text")
+        localLayout = QtGui.QVBoxLayout()
+        localLayout.addWidget(topLabel)
+        self.setLayout(localLayout)
 
 if __name__ == '__main__':
-
     import sys
-
     app = QtGui.QApplication(sys.argv)
-
-    if len(sys.argv) >= 2:
-        fileName = sys.argv[1]
-    else:
-        fileName = "."
-
-    tabdialog = TabDialog(fileName)
+    tabdialog = TabDialog()
     sys.exit(tabdialog.exec_())
