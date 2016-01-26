24af3ebe17f77fbf8f6529f1b6a74a8237638660
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-01-26 13:44:41 +0000

    shortening lines as last payment of technical debt for the moment

--- lui_testing/phil_test/code_generator_test.py
+++ lui_testing/phil_test/code_generator_test.py
@@ -126,13 +126,13 @@ class tree_2_lineal(object):
 
                 print "scope_info.f_path =", scope_info.f_path
                 scope_info.indent = scope_info.f_path.count('.')
-                print "scope_info.f_path.count('.')", scope_info.f_path.count('.')
+                print "scope_info.f_path.count('.') =", scope_info.indent
 
                 self.lst_obj.append(scope_info)
                 self.deep_in_rec(single_obj.objects)
 
             else:
-                print "\n\n _____________________ <<< WARNING neither definition or scope\n\n"
+                print "\n\n _____________ <<< WARNING neither definition or scope\n\n"
 
 
 def phil_list_2_disc(lst_obj):
@@ -147,36 +147,45 @@ def phil_list_2_disc(lst_obj):
     for obj in lst_obj:
 
         if( str(type(obj)) == "<class '__main__.ScopeData'>" ):
-            my_str = "        label_tst = QLabel(\"" + " " * obj.indent * 4 + str(obj.name)  + "\")"
+            my_str = "        label_tst = QLabel(\"" + " " * obj.indent * 4
+            my_str += str(obj.name)  + "\")"
             src_code_aut.append(my_str)
 
             if( obj.indent < 3 ):
                 f_siz = str(14 - obj.indent)
             else:
                 f_siz = "13"
-            my_str = "        label_tst.setFont(QFont(\"Monospace\", " + f_siz + ", QFont.Bold))"
+            my_str = "        label_tst.setFont(QFont(\"Monospace\", "
+            my_str += f_siz + ", QFont.Bold))"
             src_code_aut.append(my_str)
             my_str = "        bg_box.addWidget(label_tst)"
             src_code_aut.append(my_str)
 
         else:
             h_box_name = "hbox_" + str(obj.name)
-            src_code_aut.append("        " + h_box_name + " =  QHBoxLayout()")
+            my_str = "        " + h_box_name + " =  QHBoxLayout()"
+            src_code_aut.append(my_str)
             label_name = "label_" + str(obj.name)
             indent = str(obj.full_path()).count('.')
-            str_to_add = "        " + label_name + " = QLabel(\""  + " " * indent * 8 + str(obj.name)  + "\")"
-            src_code_aut.append(str_to_add)
+            my_str = "        " + label_name + " = QLabel(\""
+            my_str += " " * indent * 8 + str(obj.name)  + "\")"
+            src_code_aut.append(my_str)
 
             if( indent < 3 ):
                 f_siz = str(int((14 - indent) * 1.3))
             else:
                 f_siz = "14"
 
-            src_code_aut.append("        " + label_name + ".setFont(QFont(\"Times\"," + f_siz + ", QFont.Bold))")
-            src_code_aut.append("        " + h_box_name + ".addWidget(" + label_name + ")")
+            my_str = "        " + label_name + ".setFont(QFont(\"Times\","
+            my_str += f_siz + ", QFont.Bold))"
+            src_code_aut.append(my_str)
+            my_str = "        " + h_box_name + ".addWidget(" + label_name + ")"
+            src_code_aut.append(my_str)
             box_name = "spn_box_" + str(obj.name)
             something_else = False
-            if(obj.type.phil_type == 'float' or obj.type.phil_type == 'int' or obj.type.phil_type == 'str' ):
+            if(obj.type.phil_type == 'float' or
+               obj.type.phil_type == 'int'   or
+               obj.type.phil_type == 'str'     ):
 
                 if( obj.type.phil_type == 'float' ):
                     widget_type_str =" = QDoubleSpinBox()"
@@ -187,32 +196,40 @@ def phil_list_2_disc(lst_obj):
                 elif( obj.type.phil_type == 'str' ):
                     widget_type_str =" = QLineEdit()"
 
-                src_code_aut.append("        " + box_name + widget_type_str)
+                my_str = "        " + box_name + widget_type_str
+                src_code_aut.append(my_str)
 
-            elif( obj.type.phil_type == 'bool' ):# or obj.type.phil_type == 'choice' ):
+            elif( obj.type.phil_type == 'bool' ):
+                my_str = "        " + box_name + " = QComboBox()"
+                src_code_aut.append(my_str)
+                my_str = "        " + box_name + ".addItem(\"False\")"
+                src_code_aut.append(my_str)
+                my_str = "        " + box_name + ".addItem(\"True\")"
+                src_code_aut.append(my_str)
 
-                src_code_aut.append("        " + box_name + " = QComboBox()")
-                src_code_aut.append("        " + box_name + ".addItem(\"False\")")
-                src_code_aut.append("        " + box_name + ".addItem(\"True\")")
 
             elif( obj.type.phil_type == 'choice' ):
 
-
-                src_code_aut.append("        " + box_name + " = QComboBox()")
+                my_str = "        " + box_name + " = QComboBox()"
+                src_code_aut.append(my_str)
 
                 for opt in obj.words:
-                    src_code_aut.append("        " + box_name + ".addItem(\"" + str(opt) + "\")")
+                    my_str = "        " + box_name
+                    my_str += ".addItem(\"" + str(opt) + "\")"
+                    src_code_aut.append(my_str)
 
             else:
                 '''
-                print "__________________________________ << WARNING find something ELSE"
-                print "__________________________________ << find", obj.type.phil_type
+                print "_____________________ << WARNING find something ELSE"
+                print "_____________________ << find", obj.type.phil_type
                 '''
                 something_else = True
 
             if( something_else == False ):
-                src_code_aut.append("        " + h_box_name + ".addWidget(" + box_name + ")")
-                src_code_aut.append("        bg_box.addLayout(" + h_box_name + ")")
+                my_str = "        " + h_box_name + ".addWidget(" + box_name + ")"
+                src_code_aut.append(my_str)
+                my_str = "        bg_box.addLayout(" + h_box_name + ")"
+                src_code_aut.append(my_str)
             print "obj.full_path()    =", obj.full_path()
 
     s_code = gen_code()
