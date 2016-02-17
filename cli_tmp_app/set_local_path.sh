3e2114f02cf876617986fad0107ad48da5744c8e
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-02-17 14:07:55 +0000

    adding to the set-path script a launch code generator step

--- cli_tmp_app/set_local_path.sh
+++ cli_tmp_app/set_local_path.sh
@@ -1,3 +1,8 @@
 echo "setting up tmp path"
 export PATH=$PATH:$(pwd)
 export DUI_PATH=$(pwd)
+cd $DUI_PATH/../dui/
+echo "generating phil parameter editor"
+dials.python phil_edit_code_generator.py
+cd $DUI_PATH
+echo "done"
