25765a098990fc0071894cabcfe8d455b65ed2cb
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-03-22 15:19:04 +0000

    playing with socket , now inside a loop part 1

--- lui_testing/sockets/tweak_example_01_client.py
+++ lui_testing/sockets/tweak_example_01_client.py
@@ -1,9 +1,14 @@
 import socket
 
-clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.connect(('localhost', 8089))
+
 
 while True:
+
+    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientsocket.connect(('localhost', 8089))
     a = str(raw_input())
     print "a =", a
     clientsocket.send(a)
+    clientsocket.close()
+    if( a == "stop" ):
+        break
