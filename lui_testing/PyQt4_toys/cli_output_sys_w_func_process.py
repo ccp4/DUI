79976c14520a0a2c460c2fc3f04550ab880606a1
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-05-18 17:51:01 +0100

    playing with tablee and process

--- lui_testing/PyQt4_toys/cli_output_sys_w_func_process.py
+++ lui_testing/PyQt4_toys/cli_output_sys_w_func_process.py
@@ -1,11 +1,16 @@
 import subprocess
 import sys
+
+
+def test_event_output(line_prn):
+    print ">>:", line_prn
+
 print "before subprocess"
 p = subprocess.Popen("./sec_interval.sh", stdout = subprocess.PIPE, bufsize = 1, shell = True)
 print "after subprocess"
 for line in iter(p.stdout.readline, b''):
     single_line = line[0:len(line)-1]
-    print single_line
+    test_event_output(single_line)
 
 p.stdout.close()
 p.wait()
