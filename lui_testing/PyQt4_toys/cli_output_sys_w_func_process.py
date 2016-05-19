30cffcad34508431175e1ead5e4175eddba78605
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-05-19 12:27:59 +0100

    working on python subprocess and outputs

--- lui_testing/PyQt4_toys/cli_output_sys_w_func_process.py
+++ lui_testing/PyQt4_toys/cli_output_sys_w_func_process.py
@@ -3,16 +3,35 @@ import sys
 
 
 def test_event_output(line_prn):
-    print ">>:", line_prn
+    print "std>> ", line_prn
+
+def test_err_output(err_line_prn):
+    print "err>> ", err_line_prn
 
 print "before subprocess"
-p = subprocess.Popen("./sec_interval.sh", stdout = subprocess.PIPE, bufsize = 1, shell = True)
+p = subprocess.Popen("./sec_interval.sh", stdout = subprocess.PIPE,
+                     stderr = subprocess.PIPE, bufsize = 1, shell = True)
+
+
+'''
+cmd = "ps -A|grep 'process_name'"
+ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+output = ps.communicate()[0]
+print output
+'''
+
 print "after subprocess"
+
 for line in iter(p.stdout.readline, b''):
     single_line = line[0:len(line)-1]
     test_event_output(single_line)
 
+for errline in iter(p.stderr.readline, b''):
+    single_errline = errline[0:len(errline)-1]
+    test_err_output(single_errline)
+
 p.stdout.close()
 p.wait()
+
 
 print "after stdout.close()"
--- lui_testing/PyQt4_toys/sec_interval.sh
+++ lui_testing/PyQt4_toys/sec_interval.sh
@@ -2,8 +2,16 @@
 echo "start"
 sleep 1
 echo "second 1"
+rm a
 sleep 1
 echo "second 2"
 sleep 1
 echo "second 3"
+sleep 1
+echo "second 4"
+sleep 1
+echo "second 5"
+sleep 1
+echo "second 6"
+
 exit
