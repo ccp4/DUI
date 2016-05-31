eb948962b63fc01235656a00e35219685cf09edb
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-05-31 21:19:26 +0100

    adding python-pil pyqt example

---
+++ lui_testing/PyQt4_toys/img_layouts/pil_qt.py
@@ -0,0 +1,18 @@
+from PIL import Image
+from PySide.QtGui import QImage, QImageReader, QLabel, QPixmap, QApplication
+
+im = Image.open("centroid_diff_x.png")
+
+data = im.convert('RGBA').tostring()
+#data = im.convert("RGBA").tostring("raw", "RGBA")
+
+
+app = QApplication([])
+
+image = QImage(data, im.size[0], im.size[1], QImage.Format_ARGB32)
+pix = QPixmap.fromImage(image)
+lbl = QLabel()
+lbl.setPixmap(pix)
+lbl.show()
+
+app.exec_()
