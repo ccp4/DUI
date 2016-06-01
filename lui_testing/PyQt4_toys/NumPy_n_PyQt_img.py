92b32132f53c46fc18ef4bc86a22e2e78d51abf2
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-06-01 13:06:04 +0100

    playing with conversion from numpy to QImage

--- lui_testing/PyQt4_toys/NumPy_n_PyQt_img.py
+++ lui_testing/PyQt4_toys/NumPy_n_PyQt_img.py
@@ -7,16 +7,16 @@ from PySide.QtGui import QImage, QLabel, QPixmap, QApplication
 height = 400
 width = 400
 
-
 arr_i = np.zeros([height, width, 4], dtype=np.uint8)
 arr_i[:, :, 0:3] = 0
-
 
 arr_i[20:100, 40:200,0] = 255
 arr_i[30:150, 50:250,1] = 255
 arr_i[40:200, 60:300,2] = 255
 
+#converting to QImage
 q_img = QImage(arr_i.data, width, height, QImage.Format_RGB32)
+
 
 #building app with IMG
 app = QApplication([])
