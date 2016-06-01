483b947d66e1653b5e68ad2734487b909396768d
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-06-01 11:47:38 +0100

    adding example that does NOT need PIL for converting from numpy to QImage

---
+++ lui_testing/PyQt4_toys/NumPy_n_PyQt_img.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from PySide.QtGui import QImage, QLabel, QPixmap, QApplication
+#from PyQt4.QtGui import QImage, QLabel, QPixmap, QApplication
+
+#building array
+height = 400
+width = 400
+
+
+arr_i = np.zeros([height, width, 4], dtype=np.uint8)
+arr_i[:, :, 0:3] = 0
+
+
+arr_i[20:100, 40:200,0] = 255
+arr_i[30:150, 50:250,1] = 255
+arr_i[40:200, 60:300,2] = 255
+
+q_img = QImage(arr_i.data, width, height, QImage.Format_RGB32)
+
+#building app with IMG
+app = QApplication([])
+pix = QPixmap.fromImage(q_img)
+lbl = QLabel()
+lbl.setPixmap(pix)
+lbl.show()
+
+app.exec_()
+
--- lui_testing/PyQt4_toys/pil_n_NumPy_n_PyQt_img.py
+++ lui_testing/PyQt4_toys/pil_n_NumPy_n_PyQt_img.py
@@ -1,10 +1,19 @@
 import numpy as np
 from PIL import Image
 from PIL import ImageOps
-from PySide.QtGui import QImage, QLabel, QPixmap, QApplication
+
+#from PySide.QtGui import QImage, QLabel, QPixmap, QApplication
+from PyQt4.QtGui import QImage, QLabel, QPixmap, QApplication
 
 #building array
-arr_i = np.zeros(400 * 400 * 3).reshape(400, 400, 3)
+height = 400
+width = 400
+
+
+#arr_i = np.zeros(400 * 400 * 3).reshape(400, 400, 3)
+#arr_i = np.zeros([height, width, 4], dtype=np.uint8)
+arr_i = np.zeros([height, width, 3], dtype=np.uint8)
+
 arr_i[20:100, 40:200,0] = 255
 arr_i[30:150, 50:250,1] = 255
 arr_i[40:200, 60:300,2] = 255
@@ -18,6 +27,12 @@ img_arr_dir[:,:,1] = arr_i[:,:,1]
 img_arr_dir[:,:,0] = arr_i[:,:,2]
 img = Image.fromarray(np.uint8(img_arr_dir))
 
+'''
+
+to_review = '''
+    bgra = np.zeros([height, width, 4], dtype=np.uint8)
+    bgra[:, :, 0:3] = cv_img
+    return QtGui.QImage(bgra.data, width, height, QtGui.QImage.Format_RGB32)
 '''
 
 img = Image.fromarray(np.uint8(arr_i))
