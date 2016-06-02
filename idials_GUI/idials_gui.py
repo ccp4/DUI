a18a2c23b2382319a410fca34ecb4b8be7d9898c
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-06-02 13:33:51 +0100

    working on idials GUI part 02

--- idials_GUI/idials_gui.py
+++ idials_GUI/idials_gui.py
@@ -24,9 +24,20 @@ print "using PySide"
 #'''
 
 class MainWidget( QWidget):
+    lst_commands = [
+                    "import",
+                    "find_spots",
+                    "index",
+                    "refine"
+                    ]
+
 
     def __init__(self):
         super(MainWidget, self).__init__()
+
+        self.lst_exe_pos = 0
+        self.controller = Controller(".")
+
 
         self.btn_prv =  QPushButton('\n  Prev \n', self)
         self.btn_prv.clicked.connect(self.prv_clicked)
@@ -49,12 +60,30 @@ class MainWidget( QWidget):
 
     def go_clicked(self):
         print "go_clicked(self)"
+        cmd_to_run = self.lst_commands[self.lst_exe_pos]
+        self.controller.set_mode(cmd_to_run)
+
+        if( cmd_to_run == "import" ):
+            self.controller.set_parameters("template=../X4_wide_M1S4_2_####.cbf", short_syntax=True)
+
+        self.controller.run()
+
 
     def nxt_clicked(self):
         print "nxt_clicked(self)"
+        self.lst_exe_pos += 1
+        '''
+        if( self.lst_exe_pos > len(self.lst_commands - 1) ):
+            self.lst_exe_pos = len(self.lst_commands - 1)
+        '''
 
     def prv_clicked(self):
         print "prv_clicked(self)"
+        self.lst_exe_pos -= 1
+        '''
+        if( self.lst_exe_pos < 0 ):
+            self.lst_exe_pos = 0
+        '''
 
 if __name__ == '__main__':
     app =  QApplication(sys.argv)
