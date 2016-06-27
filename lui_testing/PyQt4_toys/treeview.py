6fef280fabb3e62bac4e741ad849cefdb0edf961
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-06-27 11:37:55 +0100

    testing treeview features

--- lui_testing/PyQt4_toys/treeview.py
+++ lui_testing/PyQt4_toys/treeview.py
@@ -2,9 +2,7 @@ from PySide.QtGui import *
 from PySide.QtCore import *
 import sys
 
-# Create a Qt application
 app = QApplication(sys.argv)
-
 # Our main window will be a QTreeView
 tree = QTreeView()
 tree.setWindowTitle('Example List')
@@ -12,26 +10,23 @@ tree.setMinimumSize(600, 400)
 
 model = QStandardItemModel(tree)
 
-foods = [
-    'Cookie dough', # Must be store-bought
-    'Hummus', # Must be homemade
-    'Spaghetti', # Must be saucy
-    'Dal makhani', # Must be spicy
-    'Chocolate whipped cream' # Must be plentiful
+lst_nums = [
+    'Uno',
+    'Dos',
+    'Tres',
+    'Cuatro',
+    'Cinco'
 ]
 
-for num, food in enumerate(foods):
+for num, elem_num in enumerate(lst_nums):
 
-    tst_new_item = QStandardItem(str(num))
+    item = QStandardItem(elem_num)
+    for n_num in xrange(num):
+        tst_new_item_01 = QStandardItem(str(n_num))
+        item.appendRow(tst_new_item_01)
 
-    item = QStandardItem(food)
-    item.appendRow(tst_new_item)
     model.appendRow(item)
 
-# Apply the model to the tree view
 tree.setModel(model)
-
-# Show the window and run the app
 tree.show()
-#app.exec_()
 sys.exit(app.exec_())
