d66fe3fce90ff4bf093dc88577a8c91897ed0b56
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-07-11 17:47:21 +0100

    adding first (empty) img viewing tool p2

---
+++ cli_tmp_app/img_view.sh
@@ -0,0 +1,4 @@
+echo "Running DUI from local copy of bzr repo"
+export BOOST_ADAPTBX_SIGNALS_DEFAULT=1
+export BOOST_ADAPTBX_FPE_DEFAULT=1
+dials.python $DUI_PATH/../img_utils/image_viewer_w_pyside.py
