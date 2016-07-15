e57afbe3e22bcb5c8b6813d4c4bd08d151f41e09
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-07-15 15:29:07 +0100

    fixing example by David

--- lui_testing/david_hack_demo.py
+++ lui_testing/david_hack_demo.py
@@ -1,26 +1,15 @@
 from dxtbx.datablock import DataBlockFactory
-datablocks = DataBlockFactory.from_json_file("datablock.json")
+datablocks = DataBlockFactory.from_json_file("/home/luiso/dui/dui_test/only_9_img/idials_tst_04/dials-1/1_import/datablock.json")
 
 datablocks[0]
 db=datablocks[0]
 
-#det=db.unique_detectors()
-
-#det=det[0]
-
-#pnl=det[0]
-
-#pnl.get_raw_data
-
-#db.extract_sweeps()
 sw=db.extract_sweeps()[0]
 
-sw.get_raw_data()
-
-sw.get_raw_data(1)
-sw.get_raw_data(2)
-sw.get_raw_data(0)
+print sw.get_raw_data(1)
+print sw.get_raw_data(2)
+print sw.get_raw_data(0)
 im1=sw.get_raw_data(0)[0]
-#im1
-#im1.all()
+
+print im1.all()
 
