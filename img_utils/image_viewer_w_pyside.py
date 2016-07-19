304ba748318c47fbcefa50998e07125b19dd3d6b
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-07-19 16:22:11 +0100

    success in this tricky thing

--- img_utils/image_viewer_w_pyside.py
+++ img_utils/image_viewer_w_pyside.py
@@ -9,33 +9,35 @@ from PySide.QtCore import *
 import numpy as np
 
 class MyDynamicLabel(QLabel):
-    def __init__(self, q_img = None):
+    def __init__(self):
         super(MyDynamicLabel, self).__init__()
+        print "From MyDynamicLabel"
 
+    def __call__(self, q_img = None):
         pix = QPixmap.fromImage(q_img)
-
         self.setPixmap(pix)
-
 
 class MyImgWin(QWidget):
 
     def __init__(self, my_array_double = None):
         super(MyImgWin, self).__init__()
 
+        self.block_3d_flex = my_array_double
+
+        self.arr_img = img_w_cpp()
         self.set_my_img(1)
 
         scrollArea = QScrollArea()
         scrollArea.setWidget(self.imageLabel)
 
         main_box = QHBoxLayout()
-
         multi_control_box = QVBoxLayout()
 
         label_test = QLabel(" <<< Test 01 >>>")
         multi_control_box.addWidget(label_test)
 
         real_time_slider = QSlider()
-        real_time_slider.setRange(0, 9)
+        real_time_slider.setRange(0, 8)
         real_time_slider.setOrientation(Qt.Horizontal)
         real_time_slider.sliderMoved.connect(self.onSliderMove)
         multi_control_box.addWidget(real_time_slider)
@@ -49,26 +51,33 @@ class MyImgWin(QWidget):
         self.show()
         self.setLayout(main_box)
 
-    def set_my_img(self, img_slice = 1):
+    def set_my_img(self, img_slice = 1, new_img = False):
 
         flex_2d_mask = flex.double(flex.grid(800, 900),0)
 
-        flex_2d_data = my_array_double[img_slice:img_slice + 1, 0:800, 0:900]
+        flex_2d_data = self.block_3d_flex[img_slice:img_slice + 1, 0:800, 0:900]
         flex_2d_data.reshape(flex.grid(800, 900))
 
-        arr_i = img_w_cpp()
-        arr_i = arr_i(flex_2d_data, flex_2d_mask)
+
+        arr_i = self.arr_img(flex_2d_data, flex_2d_mask)
 
         q_img = QImage(arr_i.data, np.size(arr_i[0:1, :, 0:1]),
                        np.size(arr_i[:, 0:1, 0:1]), QImage.Format_RGB32)
+        if new_img == False:
+            self.imageLabel = MyDynamicLabel()
 
-        self.imageLabel = MyDynamicLabel(q_img)
-
+        self.imageLabel(q_img)
 
 
     def onSliderMove(self, position = None):
         print "position =", position
         print "onSliderMove"
+
+        self.set_my_img(img_slice = position, new_img = True)
+
+        self.imageLabel.repaint()
+        self.update()
+
 
 
 if __name__ == '__main__':
