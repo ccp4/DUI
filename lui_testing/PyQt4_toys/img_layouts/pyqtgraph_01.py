61763dd7ea7634bcc49c0378341e5d1812cdc532
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-08-17 16:54:30 +0100

    adding another simple example of pyqtgraph

--- lui_testing/PyQt4_toys/img_layouts/pyqtgraph_01.py
+++ lui_testing/PyQt4_toys/img_layouts/pyqtgraph_01.py
@@ -6,7 +6,7 @@ to allow easy data inspection (but note that PySide unfortunately does not
 call the Qt event loop while the interactive prompt is running, in this case
 it is necessary to call QApplication.exec_() to make the windows appear).
 """
-
+import PySide
 import numpy as np
 import pyqtgraph as pg
 
