55617089d7cce389dc9c5d8208ecfd21e9b93ee9
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-09-13 16:04:21 +0100

    adding import widget, some work to do here yet

--- idials_GUI/custom_widgets.py
+++ idials_GUI/custom_widgets.py
@@ -26,6 +26,143 @@ try:
 except:
     from dials.command_line.export_mtz import phil_scope as phil_scope_export
 
+class ImportPage(QWidget):
+
+    '''
+    This stacked widget basically helps the user to browse the input images
+    path and the working directory, there is no auto-generated code in use
+    withing this widget and there is no multiple tabs like the other stacked
+    widgets.
+    '''
+
+    # FIXME when the user enters a path without images dials fails to import
+    # but does not raises an error consequently there is no red output in the GUI
+
+    # FIXME study the file:
+    # Dxtbx/sweep_filenames.py
+
+    def __init__(self, parent=None):
+        super(ImportPage, self).__init__(parent)
+        #self.super_parent = parent # reference across the hole GUI to MyMainDialog
+
+        #self.super_parent.w_dir = os.getcwd()
+        self.cmd_lin_default = "dials.import ~/put/your/path/here"
+        self.button_label = "   Import      "
+
+        import_path_group =  QGroupBox("Experiment IMG Directory")
+        import_path_layout =  QHBoxLayout()
+
+        self.lin_import_path =   QLineEdit(self)
+        import_path_layout.addWidget(self.lin_import_path)
+        import_path_button =  QPushButton(" \n    Find experiment Dir            . \n")
+        #import_path_button.clicked.connect(self.find_my_img_dir)
+        import_path_layout.addWidget(import_path_button)
+        import_path_group.setLayout(import_path_layout)
+
+        w_dir_group =  QGroupBox("Working Directory")
+        w_dir_layout =  QHBoxLayout()
+        self.w_dir_lin =   QLineEdit(self)
+        self.w_dir_lin.setText("/some/w_diw/here")
+        #self.w_dir_lin.setText(self.super_parent.w_dir)
+        w_dir_layout.addWidget(self.w_dir_lin)
+        w_dir_button =  QPushButton(" \n    Change Working Dir    . \n")
+        #w_dir_button.clicked.connect(self.change_w_dir)
+        w_dir_layout.addWidget(w_dir_button)
+        w_dir_group.setLayout(w_dir_layout)
+
+
+        self.auto_next_check =  QCheckBox("Enable auto-Next Feature")
+        #self.auto_next_check.stateChanged.connect(self.changed_auto_next)
+        mainLayout =  QVBoxLayout()
+        mainLayout.addWidget(import_path_group)
+        mainLayout.addWidget(w_dir_group)
+
+        '''
+        imageLabel =  QLabel()
+        #dials_logo_path = my_dui_path + "/../dui/resources/DIALS_Logo_scaled.png"
+        dials_logo_path = my_dui_path + "/../dui/resources/DIALS_Logo_smaller_centred.png"
+
+        #dials_logo_path = my_dui_path + "/../dui/resources/DIALS_Logo.png"
+        image =  QImage(dials_logo_path)
+        imageLabel.setPixmap( QPixmap.fromImage(image))
+        mainLayout.addWidget(imageLabel)
+        #imageLabel.setScaledContents(True)
+        '''
+
+
+        #mainLayout.addStretch(1)
+        mainLayout.addWidget(self.auto_next_check)
+
+
+
+        big_layout =  QHBoxLayout()
+        big_layout.addLayout(mainLayout)
+
+        '''
+        self.multi_line_txt = TextBrows()
+        big_layout.addWidget(self.multi_line_txt)
+        '''
+
+        self.setLayout(big_layout)
+        #self.show()
+
+        tmp_disabled = '''
+
+    def changed_auto_next(self):
+        print "changed_auto_next"
+        self.update_auto_next_flag()
+
+    def update_auto_next_flag(self):
+        print "self.auto_next_check.checkState() =", self.auto_next_check.checkState()
+        state = self.auto_next_check.checkState()
+        if( state == 0 ):
+            self.auto_next_flag = "unchecked"
+        else:
+            self.auto_next_flag = "checked"
+
+    def find_my_img_dir(self, event = None):
+        selected_file_path = str( QFileDialog.getOpenFileName(self, "Open IMG Dir"))
+        print "[file path found] =", selected_file_path
+
+        if( selected_file_path ):
+            for pos, single_char in enumerate(selected_file_path):
+                if( single_char == "/" or single_char == "\\" ):
+                    pos_sep = pos
+
+            dir_name = selected_file_path[:pos_sep]
+            if( dir_name[0:3] == "(u\'" ):
+                print "dir_name[0:3] == \"(u\'\""
+                dir_name = dir_name[3:]
+
+            print "dir_name(final) =", dir_name
+            self.lin_import_path.setText(dir_name)
+            self.cmd_lin_default = "dials.import "+ dir_name
+            print "CLI =", self.cmd_lin_default
+
+        else:
+            print "Failed to pick dir"
+            self.cmd_lin_default = " "
+
+        #self.super_parent.gui_line_edit.setText(self.cmd_lin_default)
+        self.super_parent.gui_line_edit.set_text(self.cmd_lin_default)
+
+    def change_w_dir(self, event = None):
+        dir_name = str( QFileDialog.getExistingDirectory(self, "Change Working Dir"))
+        print "[dir path found] =", dir_name
+
+        if( dir_name ):
+            self.super_parent.w_dir = dir_name
+            os.chdir(self.super_parent.w_dir)
+            self.w_dir_lin.setText(self.super_parent.w_dir)
+            print "dir_name(w_dir) =", self.super_parent.w_dir
+
+        else:
+            print "Failed to pick dir"
+
+
+        '''
+
+
 
 class imp_ops(QWidget):
     def __init__(self, str_label = None):
@@ -81,7 +218,7 @@ class StepList(object):
 
     def __init__(self):
         self.lst_widg  = [
-                          imp_ops("                 import"),
+                          ImportPage(),
                           ParamMainWidget(phil_scope_find_spots),
                           ParamMainWidget(phil_scope_index),
                           ParamMainWidget(phil_scope_refine_br_st),
