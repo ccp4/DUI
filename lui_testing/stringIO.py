a29cd510499aae9659662924aeddfb708f6b272c
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-10-05 01:11:26 +0100

    playing with stdout 03

--- lui_testing/stringIO.py
+++ lui_testing/stringIO.py
@@ -1,13 +1,17 @@
 from cStringIO import StringIO
 import sys
 
+def func_x(n):
+    for iterando in xrange(n):
+        print " printing time ", iterando
+
+
 old_stdout = sys.stdout
 
 mystdout = StringIO()
 sys.stdout = mystdout
 
-for iterando in xrange(5):
-    print " printing time ", iterando
+func_x(5)
 
 sys.stdout = old_stdout
 
@@ -15,3 +19,9 @@ print "\n end StringIO stuff"
 
 lst_stdout = mystdout.getvalue()
 print " >>>> \n", lst_stdout , "\n<<<<<"
+print "type(lst_stdout) =", type(lst_stdout)
+
+print "counting \\n in lst_stdout"
+for pos in lst_stdout:
+    if pos == "\n":
+        print "found \\n"
