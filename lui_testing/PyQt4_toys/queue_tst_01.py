71c35d03676804b38f52cc5a4925b5046aad192d
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-10-11 16:33:18 +0100

    nothing serious

--- lui_testing/PyQt4_toys/queue_tst_01.py
+++ lui_testing/PyQt4_toys/queue_tst_01.py
@@ -32,15 +32,13 @@ class MyReceiver(QObject):
 # An example QObject (to be run in a QThread) which outputs information with print
 class Running_iDIALS_stuff(QThread):
     def run(self):
-
         #subprocess.call("./sec_interval.sh", shell=True)
-
-
         for i in range(50):
             time.sleep(0.1)
             print i
 
         print "end for loop"
+
 
 
 # An Example application QWidget containing the textedit to redirect stdout to
