a47c3a06f89fc7978bfd68552291102492558699
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-10-19 11:41:41 +0100

    working on example for the progress bar p3

--- lui_testing/PyQt4_toys/transparent_p_bar.py
+++ lui_testing/PyQt4_toys/transparent_p_bar.py
@@ -14,7 +14,7 @@ class OverlayWidg(QWidget):
 
         self.setPalette(palette)
 
-        self.pos_n = 1
+        self.pos_n = 1.0
         self.n_of_pos = 16.0
         self.my_timer = QTimer(self)
         self.my_timer.timeout.connect(self.on_timeout)
@@ -27,26 +27,26 @@ class OverlayWidg(QWidget):
         painter.setRenderHint(QPainter.Antialiasing)
         painter.fillRect(event.rect(), QBrush(QColor(255, 255, 255, 50)))
 
-        x1 = int( (float(self.pos_n) * float(self.width()) ) / self.n_of_pos)
+        x1 = int( (self.pos_n * float(self.width()) ) / self.n_of_pos)
         y1 = 0
         h = self.height()
         w = self.width() / self.n_of_pos
         x1 -= w
 
-        rd = 1
+        rd = (self.n_of_pos - self.pos_n) / self.n_of_pos  * 255
         gr = 1
-        bl = 255
+        bl = self.pos_n / self.n_of_pos * 255
 
         painter.drawRect(x1, y1, w, h)
-        painter.fillRect(x1, y1, w, h, QColor(1, 1, 255, 127))
+        painter.fillRect(x1, y1, w, h, QColor(rd, gr, bl, 127))
 
         painter.setPen(QPen(Qt.NoPen))
 
     def update_cross(self):
         if(self.pos_n < self.n_of_pos):
-            self.pos_n += 1
+            self.pos_n += 1.0
         else:
-            self.pos_n = 1
+            self.pos_n = 1.0
 
     def on_timeout(self):
         self.update_cross()
