1c3db256020985e155a8f867ab41385e96bb211e
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-10-19 09:50:02 +0100

    adding one more example for the progress bar and disabling temporarily the image viewer

--- idials_GUI/outputs_gui.py
+++ idials_GUI/outputs_gui.py
@@ -64,13 +64,12 @@ class outputs_widget( QWidget):
         my_box = QVBoxLayout()
         v_splitter = QTabWidget()
 
-        self.img_view = MyImgWin("/home/luiso/dui/dui_test/only_9_img/dui_idials_GUI_tst_15/dials-1/1_import/datablock.json")
+        #self.img_view = MyImgWin("/home/luiso/dui/dui_test/only_9_img/dui_idials_GUI_tst_15/dials-1/1_import/datablock.json")
         self.web_view = WebTab()
-
         self.in_txt_out = TextOut()
 
 
-        v_splitter.addTab(self.img_view, "Image View")
+        #v_splitter.addTab(self.img_view, "Image View")
         v_splitter.addTab(self.web_view, "Report View")
         v_splitter.addTab(self.in_txt_out, "Log View")
 
---
+++ lui_testing/PyQt4_toys/transparent_p_bar.py
@@ -0,0 +1,87 @@
+'''
+from PySide.QtCore import *
+from PySide.QtGui import *
+'''
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+class OverlayWidg(QWidget):
+    def __init__(self, parent=None):
+        super(OverlayWidg, self).__init__(parent)
+
+        palette = QPalette(self.palette())
+        palette.setColor(palette.Background, Qt.transparent)
+
+        self.setPalette(palette)
+
+        self.pos_8 = 1
+        self.my_timer = QTimer(self)
+        self.my_timer.timeout.connect(self.on_timeout)
+        self.my_timer.start(500)
+
+
+    def paintEvent(self, event):
+        painter = QPainter()
+        painter.begin(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.fillRect(event.rect(), QBrush(QColor(255, 255, 255, 127)))
+
+        x1 = int( (float(self.pos_8) * float(self.width()) ) / 8.0)
+        y1 = 0
+        h = self.height()
+        w = self.width() / 8.0
+        x1 -= w
+
+        painter.drawRect(x1, y1, w, h)
+        painter.fillRect(x1, y1, w, h, QColor(100, 100, 255, 127))
+
+        painter.setPen(QPen(Qt.NoPen))
+
+    def update_cross(self):
+        if(self.pos_8 < 8):
+            self.pos_8 += 1
+        else:
+            self.pos_8 = 1
+
+
+    def on_timeout(self):
+        self.update_cross()
+        self.update()
+
+
+class windowOverlay(QWidget):
+    def __init__(self, parent=None):
+        super(windowOverlay, self).__init__(parent)
+
+        self.editor = QTextEdit()
+        self.editor.setPlainText("OVERLAY"*100)
+
+        self.button = QPushButton("Toggle Overlay")
+
+        self.verticalLayout = QVBoxLayout(self)
+        self.verticalLayout.addWidget(self.editor)
+        self.verticalLayout.addWidget(self.button)
+
+        self.painted_overlay = OverlayWidg(self.editor)
+        self.painted_overlay.hide()
+
+        self.button.clicked.connect(self.unpdate_w_click)
+
+    def unpdate_w_click(self):
+        if self.painted_overlay.isVisible():
+            self.painted_overlay.setVisible(False)
+        else:
+            self.painted_overlay.setVisible(True)
+            self.painted_overlay.pos_8 = 1
+
+    def resizeEvent(self, event):
+        self.painted_overlay.resize(event.size())
+        event.accept()
+
+if __name__ == "__main__":
+    import  sys
+
+    app = QApplication(sys.argv)
+    main = windowOverlay()
+    main.show()
+    sys.exit(app.exec_())
