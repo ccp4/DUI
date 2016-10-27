5b435f6378185eb3831ba115d26a99c5d68d6a83
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-10-27 10:54:52 +0100

    playing with formats

--- idials_GUI/dynamic_reindex_gui.py
+++ idials_GUI/dynamic_reindex_gui.py
@@ -9,7 +9,7 @@ def ops_list_from_json(json_path = None):
         #json_path = "../../../dui_test/X4_wide/dui_idials_test_01/dials-1/4_refine_bravais_settings/bravais_summary.json"
         #json_path = "../../../dui_test/X4_wide_first_5_img/dui_idials_test_01/dials-1/7_refine_bravais_settings/bravais_summary.json"
         #json_path = "../../../dui_test/only_9_img/dui_idials_GUI_tst_17/dials-1/8_refine_bravais_settings/bravais_summary.json"
-        print "\n No Path to bravais_summary.json provided \n"
+        #print "\n No Path to bravais_summary.json provided \n"
         return None
 
     with open(json_path) as json_file:
@@ -20,59 +20,61 @@ def ops_list_from_json(json_path = None):
     longest_str_lenght = 0
 
     for key, value in json_data.iteritems():
-        print "key =", key
-        print "\n"
-        print "value =", value
-        print "\n"
+        #print "key =", key
+        #print "\n"
+        #print "value =", value
+        #print "\n"
         #labl = ""
         for inner_key in value:
-            print "inner_key =", inner_key
-            print "inner_value =", value[inner_key]
+            #print "inner_key =", inner_key
+            #print "inner_value =", value[inner_key]
 
             if( inner_key == "rmsd" ):
                 rmsd_val = value["rmsd"]
-                rmsd_str = " {:7.3}".format(rmsd_val)
-                print "__________________________________________ type(rmsd_val) =", type(rmsd_val)
+                rmsd_str = " {:7.4}".format(rmsd_val)
+                #rmsd_str = " " + str(rmsd_val)
+
+                #print "__________________________________________ type(rmsd_val) =", type(rmsd_val)
 
             elif( inner_key == "cb_op" ):
                 cb_op_val = value["cb_op"]
                 cb_op_str = str(cb_op_val).rjust(18)
-                print "__________________________________________ type(cb_op_val) =", type(cb_op_val)
+                #print "__________________________________________ type(cb_op_val) =", type(cb_op_val)
 
             elif( inner_key ==  "min_cc" ):
                 min_cc_val = value["min_cc"]
                 min_cc_str = " {:7.3}".format(min_cc_val)
+                #print "__________________________________________ type(min_cc_val) =", type(min_cc_val)
+                #TODO the format here is not always giving the same with
 
-                print "__________________________________________ type(min_cc_val) =", type(min_cc_val)
 
             elif( inner_key ==  "max_cc" ):
                 max_cc_val = value["max_cc"]
                 max_cc_str = " {:7.3}".format(max_cc_val)
-                print "__________________________________________ type(max_cc_val) =", type(max_cc_val)
-
+                #print "__________________________________________ type(max_cc_val) =", type(max_cc_val)
+                #TODO the format here is not always giving the same with
 
             elif( inner_key == "bravais" ):
                 bravais_val = value["bravais"]
-
                 bravais_str = " " + str(bravais_val).ljust(3)
                 #bravais_str = str(bravais_val)
-                print "__________________________________________ type(bravais_val) =", type(bravais_val)
+                #print "__________________________________________ type(bravais_val) =", type(bravais_val)
 
             elif( inner_key ==  "nspots" ):
                 nspots_val = value["nspots"]
                 nspots_str =" {:9} ".format(nspots_val)
-                print "__________________________________________ type(nspots_val) =", type(nspots_val)
+                #print "__________________________________________ type(nspots_val) =", type(nspots_val)
 
             elif( inner_key ==  "max_angular_difference" ):
 
                 angular_diff_val = value["max_angular_difference"]
                 angular_diff_str = " {:7.4} ".format(angular_diff_val)
-                print "__________________________________________ type(angular_diff_val) =", type(angular_diff_val)
+                #print "__________________________________________ type(angular_diff_val) =", type(angular_diff_val)
 
             elif( inner_key ==  "correlation_coefficients" ):
                 corr_coeff_val = value["correlation_coefficients"]
                 corr_coeff_str =str(corr_coeff_val)
-                print "__________________________________________ type(corr_coeff_val) =", type(corr_coeff_val)
+                #print "__________________________________________ type(corr_coeff_val) =", type(corr_coeff_val)
 
 
             elif( inner_key ==  "unit_cell" ):
@@ -80,8 +82,8 @@ def ops_list_from_json(json_path = None):
                 uc_d = unit_cell_val[0:3]
                 uc_a = unit_cell_val[3:6]
 
-                print "uc_d =", uc_d
-                print "uc_a =", uc_a
+                #print "uc_d =", uc_d
+                #print "uc_a =", uc_a
 
 
                 unit_cell_str = "({:6.3}".format(uc_d[0]) \
@@ -92,23 +94,23 @@ def ops_list_from_json(json_path = None):
                               +  "{:7.4}".format(uc_a[1]) \
                               +  "{:7.4})".format(uc_a[2]) \
 
-                print "__________________________________________ type(unit_cell_val) =", type(unit_cell_val)
+                #print "__________________________________________ type(unit_cell_val) =", type(unit_cell_val)
 
             elif( inner_key ==  "recommended" ):
                 recommended_val = value["recommended"]
                 recommended_str = str(recommended_val)
-                print "__________________________________________ type(recommended_val) =", type(recommended_val)
+                #print "__________________________________________ type(recommended_val) =", type(recommended_val)
 
                 #'''
             elif( inner_key == "cc_nrefs" ):
                 cc_nrefs_val = value["cc_nrefs"]
                 cc_nrefs_str = str(cc_nrefs_val)
-                print "__________________________________________ type(cc_nrefs_val) =", type(cc_nrefs_val)
+                #print "__________________________________________ type(cc_nrefs_val) =", type(cc_nrefs_val)
 
                 #'''
-            print "\n"
+            #print "\n"
 
-        print "\n"
+        #print "\n"
 
         single_lin_lst = [int(key), angular_diff_str + rmsd_str + min_cc_str
               + max_cc_str + nspots_str + bravais_str +unit_cell_str + cb_op_str]
@@ -160,10 +162,6 @@ class MyReindexOpts(QWidget):
     def __call__(self):
 
         print "\n from __call__  << ReindexOpts page >> \n"
-
-        #print "dir(self.super_parent) =", dir(self.super_parent)
-        #print "dir(self.super_parent.idials_widget.controler) =", dir(self.super_parent.idials_widget)
-
         print "self.super_parent.idials_widget.controller.get_current().name =", self.super_parent.idials_widget.controller.get_current().name
 
         self.super_parent.btn_go_clicked()
