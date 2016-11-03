8870d7e19d5a024e055630f4ff6628aee08cb2ca
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-11-03 15:11:11 +0000

    removing leftover comment

--- cli_tmp_app/img_view.sh
+++ cli_tmp_app/img_view.sh
@@ -1,5 +1,4 @@
 echo "Running DUI from local copy of bzr repo"
 export BOOST_ADAPTBX_SIGNALS_DEFAULT=1
 export BOOST_ADAPTBX_FPE_DEFAULT=1
-#dials.python $DUI_PATH/../img_utils/image_viewer_w_pyside.py
 dials.python $DUI_PATH/../idials_GUI/img_viewer/image_viewer_w_opengl.py $1
