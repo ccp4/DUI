249ab4e95be5daacd3a1defd833a268f9dde3afe
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-11-08 15:08:46 +0000

    adding palette change feature

--- lui_testing/PyQt4_toys/img_from_json_file.py
+++ lui_testing/PyQt4_toys/img_from_json_file.py
@@ -10,12 +10,11 @@ class img_w_cpp(object):
     def __init__(self):
         self.wx_bmp_arr = rgb_img()
 
-    def __call__(self, flex_data_in, flex_mask_in, show_nums = False, i_min = -3.0, i_max = 200.0):
+    def __call__(self, flex_data_in, flex_mask_in, show_nums = False,
+                 i_min = -3.0, i_max = 200.0, palette = "hot ascend"):
 
         err_code = self.wx_bmp_arr.set_min_max(i_min, i_max)
-        #err_code = self.wx_bmp_arr.set_min_max(0.0, 1500.0)
 
-        palette = "hot ascend"
         if palette == "black2white":
             palette_num = 1
         elif palette == "white2black":
@@ -86,12 +85,15 @@ class ImgPainter(QWidget):
             img_paint.end()
 
 
-def build_qimg(img_flex):
+def build_qimg(img_flex, palette):
     arr_img = img_w_cpp()
+
+    #TODO remember that the previous line needs to run only once
+
 
     flex_2d_data = img_flex.as_double()
     flex_2d_mask = flex.double(flex.grid(flex_2d_data.all()[0], flex_2d_data.all()[1]), 0)
-    arr_i = arr_img(flex_2d_data, flex_2d_mask, i_min = -3.0, i_max = 500)
+    arr_i = arr_img(flex_2d_data, flex_2d_mask, i_min = 0.0, i_max = 100, palette = palette)
 
     q_img = QImage(arr_i.data, np.size(arr_i[0:1, :, 0:1]),
                    np.size(arr_i[:, 0:1, 0:1]), QImage.Format_RGB32)
@@ -104,12 +106,17 @@ class BigWidget(QWidget):
     def __init__(self):
         super(BigWidget, self).__init__()
         my_box = QVBoxLayout()
+        top_box = QHBoxLayout()
+        left_top_box = QVBoxLayout()
+        right_top_box = QVBoxLayout()
+
         self.my_painter = ImgPainter()
 
+        #json_file_path = "/home/luiso/dui/dui_test/only_9_img/dui_idials_tst_04/dials-1/1_import/datablock.json"
         json_file_path = "/home/luiso/dui/dui_test/X4_wide/test_02/dials-1/1_import/datablock.json"
         #json_file_path = "/home/lui/dui/dui_test/X4_wide/tst01/datablock.json"
-        datablocks = DataBlockFactory.from_json_file(json_file_path)
 
+        datablocks = DataBlockFactory.from_json_file(json_file_path)
         db = datablocks[0]
         self.my_sweep = db.extract_sweeps()[0]
 
@@ -119,39 +126,58 @@ class BigWidget(QWidget):
         n_of_imgs = self.my_sweep.get_array_range()[1]
         print "n_of_imgs =", n_of_imgs
 
-        self.set_img_num(0)
+        self.set_img_num(0, "hot ascend")
         self.my_painter.set_img_pix(self.current_qimg )
 
         my_scrollable = ScrollableImg(self.my_painter)
-        my_box.addWidget(my_scrollable)
+
 
         img_select = QComboBox()
-        img_select.tmp_lst=[]
+
         for num in xrange(n_of_imgs):
             labl = "image number:" + str(num)
-            img_select.tmp_lst.append(labl)
-
-        for lst_itm in img_select.tmp_lst:
-            img_select.addItem(lst_itm)
+            img_select.addItem(labl)
 
         img_select.setCurrentIndex(0)
-        img_select.currentIndexChanged.connect(self.combobox_changed)
-        my_box.addWidget(img_select)
+        img_select.currentIndexChanged.connect(self.img_changed_by_user)
+
+        palette_select = QComboBox()
+
+        self.palette_lst = ["hot ascend", "hot descend", "black2white", "white2black"]
+        for plt in self.palette_lst:
+            palette_select.addItem(plt)
+
+        palette_select.currentIndexChanged.connect(self.palette_changed_by_user)
+
+        left_top_box.addWidget(palette_select)
+        top_box.addLayout(left_top_box)
+
+        right_top_box.addWidget(img_select)
+        top_box.addLayout(right_top_box)
+
+        my_box.addLayout(top_box)
+        my_box.addWidget(my_scrollable)
 
         self.setLayout(my_box)
         self.show()
 
-    def set_img_num(self, img_n):
-        img_arr = self.my_sweep.get_raw_data(img_n)[0]
-        print "img_arr.all() =", img_arr.all()
-        self.current_qimg  = build_qimg(img_arr)
+    def set_img_num(self, img_n, palette):
+        self.img_arr = self.my_sweep.get_raw_data(img_n)[0]
+        print "self.img_arr.all() =", self.img_arr.all()
+        self.current_qimg  = build_qimg(self.img_arr, palette)
 
-    def combobox_changed(self, value):
+    def palette_changed_by_user(self, new_palette_num):
+        print "palette_num =", new_palette_num
+        print "palette =", self.palette_lst[new_palette_num]
+        self.current_qimg  = build_qimg(self.img_arr, self.palette_lst[new_palette_num])
+        self.my_painter.set_img_pix(self.current_qimg)
+
+
+    def img_changed_by_user(self, value):
         sender = self.sender()
-        print "combobox_changed to: ", str(sender.tmp_lst[value])
         print "Num =", value
 
-        self.set_img_num(value)
+        self.set_img_num(value, "hot ascend")
         self.my_painter.set_img_pix(self.current_qimg)
 
 
