257c41a37f4104dd1bc035c5b0a0e1ff5fef4412
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-11-08 16:19:08 +0000

    preparing scripts for new img viewer

--- cli_tmp_app/img_view.sh
+++
@@ -1,4 +0,0 @@
-echo "Running DUI from local copy of bzr repo"
-export BOOST_ADAPTBX_SIGNALS_DEFAULT=1
-export BOOST_ADAPTBX_FPE_DEFAULT=1
-dials.python $DUI_PATH/../idials_GUI/img_viewer/image_viewer_w_opengl.py $1
