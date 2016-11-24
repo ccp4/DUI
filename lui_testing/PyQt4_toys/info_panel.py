c9c7161213b523f05f21161665718ca4d4b5ba0f
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-11-24 14:00:21 +0000

    removing no longer needed example

--- lui_testing/PyQt4_toys/info_panel.py
+++ lui_testing/PyQt4_toys/info_panel.py
@@ -22,45 +22,12 @@ class InstrumentData(object):
         self.yb = None
         self.dd = None
 
-
-def print_someting(experiments_argv):
-
-    '''
-
-    data ='/home/luiso/dui/dui_test/X4_wide/dui_idials_tst_03/dials-1/3_index/experiments.json'
-    print_someting(data)
-
-    data ='/home/luiso/dui/dui_test/X4_wide/dui_idials_tst_03/dials-1/5_refine/experiments.json'
-    print_someting(data)
-
-
-    '''
+def update_crystal(cr_dat, experiments_path):
 
     from dxtbx.model.experiment.experiment_list import ExperimentListFactory
 
     experiments = ExperimentListFactory.from_json_file(
-                  experiments_argv, check_format=False)
-
-    print "len(experiments)", len(experiments)
-    print experiments[0]
-
-    for exp in experiments:
-        print "exp =", exp
-        #print "dir(exp) =", dir(exp), "\n\n"
-
-        #print "dir(exp.crystal) =", dir(exp.crystal)
-
-        print "exp.crystal.get_space_group =", exp.crystal.get_space_group()
-        print "exp.crystal.get_unit_cell =", exp.crystal.get_unit_cell()
-
-
-def update_crystal(cr_dat, experiments_argv):
-
-
-    from dxtbx.model.experiment.experiment_list import ExperimentListFactory
-
-    experiments = ExperimentListFactory.from_json_file(
-                  experiments_argv, check_format=False)
+                  experiments_path, check_format=False)
 
     print "len(experiments)", len(experiments)
     print experiments[0]
