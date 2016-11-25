8eb01c11cbf2681144820fd91aa241b00e404383
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-11-25 13:45:19 +0000

    commenting and spacing properly the setup script

--- dui_idials_app/setup_setpath.sh
+++ dui_idials_app/setup_setpath.sh
@@ -1,6 +1,11 @@
+# setup script to prepare setpath script
+
+# Python script the generates setpath.sh script
+# with hard-codded path to runnable tools
 dials.python write_setpath.py $(pwd)
 
-
+# making runnable the setpath script
+chmod +x setpath.sh
 
 #the next four lines need further testing before replacing $(pwd)
 #echo "Hi "
