6d309f1a0e9f4d9f876b0998e0dc63916005483c
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-12-04 22:03:34 +0000

    playing with pop_win_03

--- lui_testing/PyQt4_toys/pop_win_03.py
+++ lui_testing/PyQt4_toys/pop_win_03.py
@@ -4,9 +4,14 @@ import sys
 from PySide.QtCore import *
 from PySide.QtGui import *
 
+example = '''
+class MyDialog(QDialog):
+    def __init__(self, parent = None):
+        super(MyDialog, self).__init__(parent)
+'''
 class MyPopup(QWidget):
-    def __init__(self):
-        QWidget.__init__(self)
+    def __init__(self, parent = None):
+        super(MyPopup, self).__init__(parent)
 
     def paintEvent(self, e):
         dc = QPainter(self)
@@ -14,11 +19,12 @@ class MyPopup(QWidget):
         dc.drawLine(100, 0, 0, 100)
 
     def closeEvent(self, event):
-        print "from << closeEvent  (QWidget) >>"
+        print "<< closeEvent ( from QWidget) >>"
+
 
 class MainWindow(QMainWindow):
-    def __init__(self, *args):
-        QMainWindow.__init__(self, *args)
+    def __init__(self, parent = None):
+        super(MainWindow, self).__init__(parent)
         self.cw = QWidget(self)
         self.setCentralWidget(self.cw)
         self.btn1 = QPushButton("Click me", self.cw)
@@ -33,12 +39,10 @@ class MainWindow(QMainWindow):
         self.w.show()
 
     def closeEvent(self, event):
-        print "from << closeEvent  (QMainWindow) >>"
+        print "<< closeEvent ( from QMainWindow) >>"
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     widget = MainWindow()
     widget.show()
     sys.exit(app.exec_())
-
-
