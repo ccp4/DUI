1e19cecdc53e14f73f5e428fbd5edbc445b8392d
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2016-12-16 15:42:42 +0000

    playing with images and bitmaps

--- lui_testing/PyQt4_toys/web_html_test.py
+++ lui_testing/PyQt4_toys/web_html_test.py
@@ -10,7 +10,8 @@ app = QApplication(sys.argv)
 web = QWebView()
 #web.load(QUrl("http://google.co.uk"))
 
-web.load(QUrl("file:///home/luiso/dui/dui_test/only_9_img/dui_idials_tst_01/dials-1/17_integrate/report.html"))
+#web.load(QUrl("file:///home/luiso/dui/dui_test/only_9_img/dui_idials_tst_01/dials-1/17_integrate/report.html"))
+web.load(QUrl("file:///scratch/some_data/weak_data/html_test/dials-report.html"))
 
 #web.load(QUrl("file:///home/luiso/dui/dui_test/test_nproc_eq_8/dials-report.html"))
 
