decac9c86b666d2977e6c66e5030042df73d12c8
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2017-02-28 14:48:30 +0000

    working on the list rearrangement in C++, putting HKL values

--- lui_testing/boost_python_toy/lst_caller_01.py
+++ lui_testing/boost_python_toy/lst_caller_01.py
@@ -13,70 +13,51 @@ class flat_data(object):
 
 class dumy_class(object):
     def maximum(self):
-        return 10
+        return 589
 
 img_select = dumy_class()
 
 def ini_reflection_table(pckl_file_path = None):
     if( pckl_file_path != None ):
 
-        print "[pickle file] =", pckl_file_path
+
         table = flex.reflection_table.from_pickle(pckl_file_path)
-        print "table =", table
         local_bbox = table[5]['bbox']
-        print "local_bbox =", local_bbox
-        print "len(table) = ", len(table)
 
         n_refs = len(table)
-
         bbox_col = map(list, table["bbox"])
+        try:
+            hkl_col = map(str, table["miller_index"])
+            print hkl_col[0]
+            print hkl_col[n_refs - 1]
 
-        # in the image viewer, the img_select variable is par of the class
+        except:
+            hkl_col = []
+
+
+
+        # in the image viewer, the img_select variable is part of the class
         n_imgs = img_select.maximum()
         print "\n\n\n"
 
-        a = lst_ext.arange_list(bbox_col, n_imgs)
-        print "a =", a
+        #print "a =", a
 
         flat_data_lst = []
         if( n_imgs > 0 ):
 
-
-            #old_stable_way = '''
             firts_time = time_now()
+            a = lst_ext.arange_list(bbox_col, hkl_col, n_imgs)
 
-            for img_num in xrange(n_imgs):
-                tmp_lst = []
-                flat_data_lst.append(tmp_lst)
 
-            for i in xrange(n_refs):
-                local_bbox = table[i]['bbox']
-                z_boud = local_bbox[4:6]
-                x_ini = local_bbox[0]
-                y_ini = local_bbox[2]
-                width = local_bbox[1] - local_bbox[0]
-                height = local_bbox[3] - local_bbox[2]
-
-                try:
-                    local_hkl = str(table[i]['miller_index'])
-
-                except:
-                    local_hkl = None
-
-                for idx in xrange( int(z_boud[0]), int(z_boud[1]) ):
-                    reflection_data = flat_data()
-                    reflection_data.box = [x_ini, y_ini, width, height]
-
-                    reflection_data.hkl = local_hkl
-
-                    flat_data_lst[idx].append(reflection_data)
-
+            print a
             print "\n building flat_data_lst (diff time) =", time_now() - firts_time, "\n"
-            #'''
+
     else:
         flat_data_lst = [None]
 
 
+
 if( __name__ == "__main__" ):
     lst_data = ini_reflection_table("/home/luiso/dui/dui_test/only_9_img/dui_idials_tst_05/dials-1/3_index/reflections.pickle")
-
+    #lst_data = ini_reflection_table("/home/luiso/dui/dui_test/th_9_3_0588/dui_idials_tst_06/dials-1/7_integrate/reflections.pickle")
+    #lst_data = ini_reflection_table("/home/luiso/dui/dui_test/only_9_img/dui_idials_tst_05/dials-1/2_find_spots/reflections.pickle")
--- lui_testing/boost_python_toy/lst_ext.cpp
+++ lui_testing/boost_python_toy/lst_ext.cpp
@@ -30,7 +30,7 @@ py::list square(py::list num_lst)
     return points;
 }
 
-py::list arange_list(py::list bbox_lst, int n_imgs)
+py::list arange_list(py::list bbox_lst, py::list hkl_lst, int n_imgs)
 {
     /*
      * This is the function that we actually need.
@@ -41,23 +41,6 @@ py::list arange_list(py::list bbox_lst, int n_imgs)
      */
     std::cout << "n_imgs =" << n_imgs << "\n";
 
-    /*
-     * Translating:
-     *
-            for i in xrange(n_refs):
-                local_bbox = table[i]['bbox']
-                z_boud = local_bbox[4:6]
-                x_ini = local_bbox[0]
-                y_ini = local_bbox[2]
-                width = local_bbox[1] - local_bbox[0]
-                height = local_bbox[3] - local_bbox[2]
-
-
-                for idx in xrange( int(z_boud[0]), int(z_boud[1]) ):
-                    reflection_data = flat_data()
-                    reflection_data.box = [x_ini, y_ini, width, height]
-    */
-
     int x_ini, y_ini, width, height;
     py::list img_lst, ref_box, tmp_lst, box_dat;
 
@@ -65,6 +48,7 @@ py::list arange_list(py::list bbox_lst, int n_imgs)
     for (int i = 0; i < n_imgs; i++){
         img_lst.append(py::list());
     }
+    py::str local_hkl;
 
     for (int i = 0; i < len(bbox_lst); i++){
         ref_box = py::extract<py::list>(bbox_lst[i]);
@@ -78,6 +62,19 @@ py::list arange_list(py::list bbox_lst, int n_imgs)
         box_dat.append(y_ini);
         box_dat.append(width);
         box_dat.append(height);
+
+
+        if( len(hkl_lst) <= 1 ){
+            local_hkl = "";
+            box_dat.append(local_hkl);
+        } else {
+            //box_dat.append(py::extract<std::string>(hkl_lst[i]));
+            local_hkl = py::extract<py::str>(hkl_lst[i]);
+            if(local_hkl == "(0, 0, 0)"){
+                local_hkl = "NO Index";
+            }
+            box_dat.append(local_hkl);
+        }
 
         for (int idx = py::extract<int>(ref_box[4]);
              idx < py::extract<int>(ref_box[5]);
@@ -95,7 +92,7 @@ BOOST_PYTHON_MODULE(lst_ext)
     def("greet", greet);
     def("give_int", give_int);
     def("square", square);
-    def("arange_list", arange_list, arg("bbox_lst"), arg("n_imgs"));
+    def("arange_list", arange_list, arg("bbox_lst"), arg("hkl_lst"), arg("n_imgs"));
     //def("arange_list", &arange_list, arg("bbox_lst"), arg("n_imgs"));
 
 
