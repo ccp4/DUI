ce6335c5f7b0efb61a1dbe66952c748105dcb0f9
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2017-03-07 14:19:31 +0000

    building tools for compiling when config runs

--- dui_idials_app/config_DUI.sh
+++ dui_idials_app/config_DUI.sh
@@ -2,7 +2,13 @@
 
 # Python script the generates setpath.sh script
 # with hard-codded path to runnable tools
-dials.python write_setpath.py $(pwd)
+MY_WD = $(pwd)
+dials.python write_setpath.py $MY_WD
 
 # making runnable the setpath script
 chmod +x setpath.sh
+
+#Attemplting to compile list C++ extension
+cd ../idials_GUI/outputs_n_viewers
+dials.python compyling_boost_ext.py
+cd $MY_WD
