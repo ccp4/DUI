d30a70584a4595062cd4d086a514e1eb1a0b8a6d
Andrey Lebedev <andrey.lebedev@stfc.ac.uk>
2017-03-27 16:33:31 +0100

    config_DUI.sh can now be run from any cwd

--- dui_idials_app/config_DUI.sh
+++ dui_idials_app/config_DUI.sh
@@ -1,14 +1,12 @@
+#!/bin/bash -xe
 # setup script to prepare setpath script
 
 # Python script the generates setpath.sh script
 # with hard-codded path to runnable tools
-MY_WD = $(pwd)
-dials.python write_setpath.py $(pwd)
-
-# making runnable the setpath script
-chmod +x setpath.sh
+MY_WD=$(cd $(dirname ${0}); pwd)
+dials.python ${MY_WD}/write_setpath.py ${MY_WD}
 
 #Attemplting to compile list C++ extension
-cd ../idials_GUI/outputs_n_viewers
+cd ${MY_WD}/../idials_GUI/outputs_n_viewers
 dials.python compyling_boost_ext.py
-cd $MY_WD
+cd ${MY_WD}
