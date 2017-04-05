a1529fe371accc36a46db9536ff5b06928d8b942
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2017-04-05 13:44:33 +0100

    organizing some parts of the code

--- idials_GUI/outputs_n_viewers/img_viewer.py
+++ idials_GUI/outputs_n_viewers/img_viewer.py
@@ -21,15 +21,18 @@ copyright (c) CCP4 - DLS
 #along with this program; if not, write to the Free Software
 #Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-import sys, os
-import numpy as np
-from dxtbx.datablock import DataBlockFactory
-from dials.array_family import flex
-from dials_viewer_ext import rgb_img
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 
+import sys, os
+import numpy as np
+
 from dxtbx.datablock import DataBlockFactory
+from dials.array_family import flex
+from dxtbx.datablock import DataBlockFactory
+
+from img_view_tools import img_w_cpp, build_qimg
+
 try:
     import lst_ext
     print "running C++ lst_ext"
@@ -54,41 +57,6 @@ MyQWidgetWithQPainter = QWidget
 class flat_data(object):
     box = None
     hkl = None
-
-
-class img_w_cpp(object):
-    def __init__(self):
-        self.wx_bmp_arr = rgb_img()
-
-    def __call__(self, flex_data_in, flex_mask_in, show_nums = False,
-                 i_min = -3.0, i_max = 200.0, palette = "hot ascend"):
-
-        err_code = self.wx_bmp_arr.set_min_max(i_min, i_max)
-
-        if palette == "black2white":
-            palette_num = 1
-        elif palette == "white2black":
-            palette_num = 2
-        elif palette == "hot ascend":
-            palette_num = 3
-        else: # assuming "hot descend"
-            palette_num = 4
-
-        img_array_tmp = self.wx_bmp_arr.gen_bmp(flex_data_in, flex_mask_in, show_nums, palette_num)
-
-        np_img_array = img_array_tmp.as_numpy_array()
-
-        height = np.size(np_img_array[:, 0:1, 0:1])
-        width = np.size( np_img_array[0:1, :, 0:1])
-
-        img_array = np.zeros([height, width, 4], dtype=np.uint8)
-
-        #for some strange reason PyQt4 needs to use RGB as BGR
-        img_array[:,:,0:1] = np_img_array[:,:,2:3]
-        img_array[:,:,1:2] = np_img_array[:,:,1:2]
-        img_array[:,:,2:3] = np_img_array[:,:,0:1]
-
-        return img_array
 
 
 def py_find_closer_hkl_func(x_mouse_scaled, y_mouse_scaled, flat_data_lst):
@@ -346,21 +314,6 @@ class ImgPainter(MyQWidgetWithQPainter):
                     '''
 
             painter.end()
-
-
-class build_qimg(object):
-    def __init__(self):
-        self.arr_img = img_w_cpp()
-
-    def __call__ (self, img_flex, palette_in, min_i, max_i):
-        flex_2d_data = img_flex.as_double()
-        flex_2d_mask = flex.double(flex.grid(flex_2d_data.all()[0], flex_2d_data.all()[1]), 0)
-        arr_i = self.arr_img(flex_2d_data, flex_2d_mask, i_min = min_i, i_max = max_i, palette = palette_in)
-
-        q_img = QImage(arr_i.data, np.size(arr_i[0:1, :, 0:1]),
-                       np.size(arr_i[:, 0:1, 0:1]), QImage.Format_RGB32)
-
-        return q_img
 
 
 class PopImgChange(QMenu):
