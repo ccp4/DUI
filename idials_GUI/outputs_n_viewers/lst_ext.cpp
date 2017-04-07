f7b3029306f906067ca65f16fd96180432270430
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2017-04-07 16:44:19 +0100

    working on a way to show boxes from added images, C++ version

--- idials_GUI/outputs_n_viewers/lst_ext.cpp
+++ idials_GUI/outputs_n_viewers/lst_ext.cpp
@@ -8,48 +8,47 @@ char const* greet()
     return "hello, world";
 }
 
-int find_closer_hkl_func(float x_mouse_scaled, float y_mouse_scaled, py::list flat_data_lst){
+py::list find_closer_hkl_func(float x_mouse_scaled, float y_mouse_scaled, py::list flat_data_lst){
     /*
      * finding the closer reflection from an X,Y position in one image
      */
 
     int hkl_result = -1;
+    int slice_result = -1;
     float dst_squared = 999999.0;
-    /*
-     * Not needed for now: debugging info
-     *
-    std::cout << "\n x_mouse_scaled = " << x_mouse_scaled
-              << "\n y_mouse_scaled = " << y_mouse_scaled
-              << "\n len(flat_data_lst) = " << len(flat_data_lst)
-              << "\n";
-
-    std::cout << "\n Using C++ search for closer reflection \n";
-    */
-    py::list reflection;
     float x, y, tmp_dst_squared, dx, dy;
     float reflection_0, reflection_1, reflection_2, reflection_3;
 
-    for (int i = 0; i < len(flat_data_lst); i++){
-        reflection = py::extract<py::list>(flat_data_lst[i]);
+    py::list reflection, img_flat_data;
 
-        reflection_0 = py::extract<float>(reflection[0]);
-        reflection_1 = py::extract<float>(reflection[1]);
-        reflection_2 = py::extract<float>(reflection[2]);
-        reflection_3 = py::extract<float>(reflection[3]);
+    for (int j = 0; j < len(flat_data_lst); j++){
+        img_flat_data = py::extract<py::list>(flat_data_lst[j]);
 
-        x = reflection_0 + reflection_2 / 2.0;
-        y = reflection_1 + reflection_3 / 2.0;
-        dx = x - x_mouse_scaled;
-        dy = y - y_mouse_scaled;
-        tmp_dst_squared = dx * dx + dy * dy;
+        for (int i = 0; i < len(img_flat_data); i++){
+            reflection = py::extract<py::list>(img_flat_data[i]);
 
-        if( tmp_dst_squared < dst_squared ){
-            hkl_result = i;
-            dst_squared = tmp_dst_squared;
+            reflection_0 = py::extract<float>(reflection[0]);
+            reflection_1 = py::extract<float>(reflection[1]);
+            reflection_2 = py::extract<float>(reflection[2]);
+            reflection_3 = py::extract<float>(reflection[3]);
+
+            x = reflection_0 + reflection_2 / 2.0;
+            y = reflection_1 + reflection_3 / 2.0;
+            dx = x - x_mouse_scaled;
+            dy = y - y_mouse_scaled;
+            tmp_dst_squared = dx * dx + dy * dy;
+
+            if( tmp_dst_squared < dst_squared ){
+                hkl_result = i;
+                slice_result = j;
+                dst_squared = tmp_dst_squared;
+            }
         }
     }
-
-    return hkl_result;
+    py::list lst_result;
+    lst_result.append(hkl_result);
+    lst_result.append(slice_result);
+    return lst_result;
 }
 
 py::list arrange_list(py::list bbox_lst, py::list hkl_lst, int n_imgs){
