2ab5034ea2b7e27018a7f4542292801dfd47e9a3
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2017-05-04 16:33:54 +0100

    hacking new web viewer toy part 2

--- lui_testing/PyQt4_toys/web_html_test_w_widget_02.py
+++ lui_testing/PyQt4_toys/web_html_test_w_widget_02.py
@@ -3,28 +3,6 @@ import sys
 #from PySide import QtCore, QtGui, QtWebKit
 from PyQt4 import QtCore, QtGui, QtWebKit
 
-class inner_widg( QtGui.QWidget):
-
-    def __init__(self, parent):
-        super(inner_widg, self).__init__()
-        self.parent_widget = parent
-
-        self.btn_go = QtGui.QPushButton('\n      Go   \n', self)
-        self.btn_go.clicked.connect(self.B_go_clicked)
-
-        hbox =  QtGui.QHBoxLayout()
-        hbox.addWidget(self.btn_go)
-
-        bg_box =  QtGui.QVBoxLayout(self)
-        bg_box.addLayout(hbox)
-
-        self.setLayout(bg_box)
-        self.show()
-
-    def B_go_clicked(self):
-        print"\n Ok    from inner_widg \n"
-        self.parent_widget.to_be_caled_from_son_widg(4)
-
 class WebWidget( QtWebKit.QWebView):
 
     def __init__(self):
@@ -40,19 +18,19 @@ class MainWidget( QtGui.QWidget):
 
         hbox =  QtGui.QHBoxLayout()
 
-        self.btn_go = inner_widg(self)
+        self.btn_go = QtGui.QPushButton('\n      Go   \n', self)
+        self.btn_go.clicked.connect(self.B_go_clicked)
         hbox.addWidget(self.btn_go)
         self.web = WebWidget()
         hbox.addWidget(self.web)
 
-        #self.setGeometry(1100, 200, 550, 250)
         self.setLayout(hbox)
         self.setWindowTitle('Shell dialog')
         self.show()
 
-    def to_be_caled_from_son_widg(self, n):
-        print "n =", n
-        print "from parent parent_widget"
+    def B_go_clicked(self):
+        print"\n go clicked \n"
+
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
