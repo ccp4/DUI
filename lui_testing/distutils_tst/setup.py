faec0a4239a00d6ade6b32d5f9562141d610851e
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2017-05-12 16:49:53 +0100

    playing with distutils

--- lui_testing/distutils_tst/setup.py
+++ lui_testing/distutils_tst/setup.py
@@ -1,5 +1,17 @@
 from distutils.core import setup
 setup(name='hi_tst',
       version='1.0',
-      py_modules=['h_w'],
-      )
+      description='test with simple Hello w...',
+      author='Luiso',
+      author_email='luis.fuentes-montero@diamond.ac.uk',
+     )
+to_add_latter = '''
+    url='https://www.python.org/sigs/distutils-sig/',
+    packages=['distutils', 'distutils.command'],
+    '''
+
+old_test = '''
+    (name='hi_tst',
+    version='0.90',
+    py_modules=['h_w'],
+    )'''
