d6998837dba2a430070967f1481adc6310d38ad2
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2017-05-12 15:39:02 +0100

    adding first test toy for distutils

---
+++ lui_testing/distutils_tst/h_w.py
@@ -0,0 +1,5 @@
+def hi_msg():
+    print "Hi there"
+
+if(__name__ == "__main__"):
+    hi_msg()
---
+++ lui_testing/distutils_tst/setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+setup(name='hi_tst',
+      version='1.0',
+      py_modules=['h_w'],
+      )
