3bd6ffe1952ca5da33c582b0207687b31ad11541
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2017-06-15 13:04:28 +0100

    adding test utf-8 code

---
+++ lui_testing/python_mic/test_char_symb.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+test = '''
+├──── include
+│      ├────  foo
+│      └────  bar
+'''
+
+if( __name__ == "__main__" ):
+    print "Hi"
+    print "test =", test
+
