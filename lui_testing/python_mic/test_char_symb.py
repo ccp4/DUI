3161152144a3194551221a75225ae8a36ac6fe81
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2017-06-15 15:00:20 +0100

    adding tree navigation test

--- lui_testing/python_mic/test_char_symb.py
+++ lui_testing/python_mic/test_char_symb.py
@@ -7,7 +7,14 @@ test = '''
 │      └────  bar
 '''
 
+ver_lin = "│"
+hor_lin = "─"
+lai_tee = "├"
+
 if( __name__ == "__main__" ):
     print "Hi"
     print "test =", test
+    print "ver_lin =", ver_lin
+    print "hor_lin =", hor_lin
+    print "lai_tee =", lai_tee
 
