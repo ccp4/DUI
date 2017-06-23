5ffcfa9527ed931dd5b82ce4539846d4789b0432
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2017-06-23 15:05:21 +0100

    playing with examples to test stderr , stdout and popen

--- lui_testing/python_mic/sec_interval_n_err.sh
+++ lui_testing/python_mic/sec_interval_n_err.sh
@@ -4,8 +4,6 @@ sleep 1
 echo "second 1"
 sleep 1
 echo "second 2"
-# suposed to fail next line
-dials.find_spots
 sleep 1
 echo "second 3"
 sleep 1
@@ -14,3 +12,21 @@ sleep 1
 echo "second 5"
 sleep 1
 echo "second 6"
+# suposed to fail next line
+dials.find_spots
+sleep 1
+echo "second 7"
+sleep 1
+echo "second 8"
+sleep 1
+echo "second 9"
+sleep 1
+echo "second 10"
+sleep 1
+echo "second 11"
+sleep 1
+echo "second 12"
+sleep 1
+echo "second 13"
+sleep 1
+echo "second 14"
