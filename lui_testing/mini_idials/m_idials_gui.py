43962373072567a0ae7ec23aa13b65667a58873b
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2017-06-27 16:14:37 +0100

    connecting new mini GUI to mini iDIALS part 1

--- lui_testing/mini_idials/m_idials_gui.py
+++ lui_testing/mini_idials/m_idials_gui.py
@@ -3,11 +3,30 @@ from PyQt4.QtCore import *
 from PyQt4.QtWebKit import *
 
 import sys
+import pickle
+from cli_utils import TreeShow
+from m_idials import Runner
 
 class MainWidget(QMainWindow):
     def __init__(self):
         super(MainWidget, self).__init__()
         self.super_parent = self
+
+        ###############################################################################################
+
+
+        self.cli_tree_output = TreeShow()
+
+        try:
+            with open ('bkp.pickle', 'rb') as bkp_in:
+                self.uni_controler = pickle.load(bkp_in)
+
+        except:
+            self.uni_controler = Runner()
+
+        self.cli_tree_output(self.uni_controler)
+
+        ###############################################################################################
 
         main_box = QHBoxLayout()
         main_box.addWidget(QLabel("DIALS command: "))
@@ -22,8 +41,16 @@ class MainWidget(QMainWindow):
         self.setCentralWidget(self.main_widget)
 
     def cmd_entr(self):
-        new_cmd = self.cmd_edit.text()
+        new_cmd = str(self.cmd_edit.text())
         print "command entered:", new_cmd
+
+        self.uni_controler.run(new_cmd)
+        self.cli_tree_output(self.uni_controler)
+
+        with open('bkp.pickle', 'wb') as bkp_out:
+            pickle.dump(self.uni_controler, bkp_out)
+
+
 
 if __name__ == '__main__':
     app =  QApplication(sys.argv)
@@ -31,3 +58,40 @@ if __name__ == '__main__':
     ex.show()
     sys.exit(app.exec_())
 
+
+
+'''
+
+if( __name__ == "__main__"):
+    tree_output = TreeShow()
+
+    try:
+        with open ('bkp.pickle', 'rb') as bkp_in:
+            uni_controler = pickle.load(bkp_in)
+
+    except:
+        uni_controler = Runner()
+
+    tree_output(uni_controler)
+
+    command = ""
+    while( command.strip() != 'exit' and command.strip() != 'quit' ):
+        try:
+            inp_str = "lin [" + str(uni_controler.current) + "] >>> "
+            command = str(raw_input(inp_str))
+            if( command == "" ):
+                print "converting empty line in self.slist()"
+                command = "slist"
+
+        except:
+            print " ... interrupting"
+            sys.exit(0)
+
+        uni_controler.run(command)
+        tree_output(uni_controler)
+
+        with open('bkp.pickle', 'wb') as bkp_out:
+            pickle.dump(uni_controler, bkp_out)
+
+
+'''
