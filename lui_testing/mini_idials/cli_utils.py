d68a1a78b4976d932bf658ae8ede399034979309
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2017-06-27 14:41:11 +0100

    remaing with CamelCase properly

--- lui_testing/mini_idials/cli_utils.py
+++ lui_testing/mini_idials/cli_utils.py
@@ -1,4 +1,4 @@
-def prin_lst(lst, curr):
+def print_list(lst, curr):
     print "__________________________listing:"
     for uni in lst:
         stp_str = str(uni.lin_num) + " comm: " + str(uni.command)
@@ -22,3 +22,79 @@ def prin_lst(lst, curr):
             stp_str += "                           <<< here I am <<<"
 
         print stp_str
+
+class TreeShow(object):
+    def __init__(self):
+        self.ind_spc = "      "
+        self.ind_lin = "------"
+
+    def __call__(self, my_runner):
+        print
+        print "status "
+        print " |  lin num "
+        print " |   |  command "
+        print " |   |   | "
+        print "------------------"
+        self.max_indent = 0
+        self.str_lst = []
+        self.add_tree(step = my_runner.step_list[0], indent = 0)
+        self.tree_print(my_runner.current)
+        print "---------------------" + self.max_indent * self.ind_lin
+
+    def tree_print(self, curr):
+        tree_dat = []
+        for tmp_lst in self.str_lst:
+            tree_dat.append(tmp_lst)
+
+        for pos, loc_lst in enumerate(tree_dat):
+            if(pos > 0):
+                if( loc_lst[1] < tree_dat[pos - 1][1] ):
+                    for up_pos in xrange(pos - 1, 0, -1):
+                        pos_in_str = loc_lst[1] * len(self.ind_spc) + 9
+                        left_side = tree_dat[up_pos][0][0:pos_in_str]
+                        right_side = tree_dat[up_pos][0][pos_in_str + 1:]
+                        if( tree_dat[up_pos][1] > loc_lst[1] ):
+                            tree_dat[up_pos][0] = left_side + "|" + right_side
+
+                        elif( tree_dat[up_pos][1] == loc_lst[1] ):
+                            break
+
+            if( loc_lst[2] == curr ):
+                lng = len(self.ind_spc) * self.max_indent + 22
+                lng_lft = lng - len(tree_dat[pos][0])
+                str_here = lng_lft * " "
+                tree_dat[pos][0] += str_here + "   <<< here "
+
+        for prn_str in tree_dat:
+            print prn_str[0]
+
+    def add_tree(self, step = None, indent = None):
+        if( step.success == True ):
+            stp_prn = " S "
+
+        elif( step.success == False ):
+            stp_prn = " F "
+
+        else:
+            stp_prn = " N "
+
+        str_lin_num = "{0:3}".format(int(step.lin_num))
+
+        stp_prn += str_lin_num + self.ind_spc * indent + "   \___"
+        try:
+            stp_prn += str(step.command[0])
+
+        except:
+            stp_prn += "None"
+
+        self.str_lst.append([stp_prn, indent, int(step.lin_num)])
+        try:
+            for line in step.next_step_list:
+                new_indent = indent + 1
+                self.add_tree(step = line, indent = new_indent)
+
+        except:
+            new_indent = int(new_indent)
+            if( new_indent > self.max_indent ):
+                self.max_indent = new_indent
+
--- lui_testing/mini_idials/m_idials.py
+++ lui_testing/mini_idials/m_idials.py
@@ -2,7 +2,7 @@
 import sys
 import subprocess
 import pickle
-from cli_utils import prin_lst
+from cli_utils import print_list, TreeShow
 class DialsCommand(object):
     def __init__(self):
         print "creating new DialsCommand (obj)"
@@ -24,7 +24,7 @@ class DialsCommand(object):
 
         return local_success
 
-class uni_step(object):
+class UniStep(object):
     dials_com_lst = [
     'import',
     'find_spots',
@@ -131,12 +131,12 @@ class uni_step(object):
 
         print "\n self.cmd_lst_to_run =", self.cmd_lst_to_run, "\n"
 
-class runner(object):
+class Runner(object):
 
     ctrl_com_lst = ["goto", "fail", "slist","reset"]
     def __init__(self):
 
-        root_node = uni_step(None)
+        root_node = UniStep(None)
         root_node.success = True
         root_node.command = ["Root"]
         self.step_list = [root_node]
@@ -167,7 +167,7 @@ class runner(object):
 
 
     def create_step(self, prev_step):
-        new_step = uni_step(prev_step)
+        new_step = UniStep(prev_step)
         self.bigger_lin += 1
         new_step.lin_num = self.bigger_lin
         try:
@@ -197,98 +197,22 @@ class runner(object):
 
     def slist(self):
         print "printing in steps list mode: \n"
-        prin_lst(self.step_list, self.current)
-
-class tree_show(object):
-    def __init__(self):
-        self.ind_spc = "      "
-        self.ind_lin = "------"
-
-    def __call__(self, my_runner):
-        print
-        print "status "
-        print " |  lin num "
-        print " |   |  command "
-        print " |   |   | "
-        print "------------------"
-        self.max_indent = 0
-        self.str_lst = []
-        self.add_tree(step = my_runner.step_list[0], indent = 0)
-        self.tree_print(my_runner.current)
-        print "---------------------" + self.max_indent * self.ind_lin
-
-    def tree_print(self, curr):
-        tree_dat = []
-        for tmp_lst in self.str_lst:
-            tree_dat.append(tmp_lst)
-
-        for pos, loc_lst in enumerate(tree_dat):
-            if(pos > 0):
-                if( loc_lst[1] < tree_dat[pos - 1][1] ):
-                    for up_pos in xrange(pos - 1, 0, -1):
-                        pos_in_str = loc_lst[1] * len(self.ind_spc) + 9
-                        left_side = tree_dat[up_pos][0][0:pos_in_str]
-                        right_side = tree_dat[up_pos][0][pos_in_str + 1:]
-                        if( tree_dat[up_pos][1] > loc_lst[1] ):
-                            tree_dat[up_pos][0] = left_side + "|" + right_side
-
-                        elif( tree_dat[up_pos][1] == loc_lst[1] ):
-                            break
-
-            if( loc_lst[2] == curr ):
-                lng = len(self.ind_spc) * self.max_indent + 22
-                lng_lft = lng - len(tree_dat[pos][0])
-                str_here = lng_lft * " "
-                tree_dat[pos][0] += str_here + "   <<< here "
-
-        for prn_str in tree_dat:
-            print prn_str[0]
-
-    def add_tree(self, step = None, indent = None):
-        if( step.success == True ):
-            stp_prn = " S "
-
-        elif( step.success == False ):
-            stp_prn = " F "
-
-        else:
-            stp_prn = " N "
-
-        str_lin_num = "{0:3}".format(int(step.lin_num))
-
-        stp_prn += str_lin_num + self.ind_spc * indent + "   \___"
-        try:
-            stp_prn += str(step.command[0])
-
-        except:
-            stp_prn += "None"
-
-        self.str_lst.append([stp_prn, indent, int(step.lin_num)])
-        try:
-            for line in step.next_step_list:
-                new_indent = indent + 1
-                self.add_tree(step = line, indent = new_indent)
-
-        except:
-            new_indent = int(new_indent)
-            if( new_indent > self.max_indent ):
-                self.max_indent = new_indent
+        print_list(self.step_list, self.current)
 
 if( __name__ == "__main__"):
-    tree_output = tree_show()
+    tree_output = TreeShow()
 
     try:
         with open ('bkp.pickle', 'rb') as bkp_in:
             uni_controler = pickle.load(bkp_in)
 
     except:
-        uni_controler = runner()
+        uni_controler = Runner()
 
     tree_output(uni_controler)
 
     command = ""
     while( command.strip() != 'exit' and command.strip() != 'quit' ):
-
         try:
             inp_str = "lin [" + str(uni_controler.current) + "] >>> "
             command = str(raw_input(inp_str))
