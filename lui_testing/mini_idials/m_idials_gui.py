5b25f6d8aaada220b9a3f8b2af54c98606f792ed
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2017-06-28 13:47:19 +0100

    finally properly connecting std output to GUI

--- lui_testing/mini_idials/m_idials_gui.py
+++ lui_testing/mini_idials/m_idials_gui.py
@@ -14,10 +14,9 @@ class DialsCommandGUI(QObject):
 
     mysignal = pyqtSignal(str)
 
-    def __init__(self):
+    def __init__(self, parent = None):
         super(DialsCommandGUI, self).__init__()
-        print "creating new DialsCommand (PyQt4 obj)"
-
+        self.my_parent = parent
 
     def __call__(self, lst_cmd_to_run):
         try:
@@ -31,10 +30,11 @@ class DialsCommandGUI(QObject):
 
             for line in iter(my_process.stdout.readline, b''):
                 single_line = line[0:len(line)-1]
-                print single_line
-                self.mysignal.emit(single_line)
+                #print single_line
+                #self.mysignal.emit(single_line)
+                self.mysignal.emit(line)
 
-            my_process.wait()
+            #my_process.wait()
             my_process.stdout.close()
             print "after ...close()"
             if( my_process.poll() == 0 ):
@@ -56,7 +56,7 @@ class MainWidget(QMainWindow):
         self.super_parent = self
         self.cli_tree_output = TreeShow()
 
-
+        #TODO try to make this object/pickle compatible with C.L.I. app
         #try:
         #    with open ('bkp.pickle', 'rb') as bkp_in:
         #        self.uni_controler = pickle.load(bkp_in)
@@ -70,17 +70,30 @@ class MainWidget(QMainWindow):
 
         self.cli_tree_output(self.uni_controler)
 
-        main_box = QHBoxLayout()
-        main_box.addWidget(QLabel("DIALS command: "))
+        main_box = QVBoxLayout()
+
+        self.textedit = QTextEdit()
+        main_box.addWidget(self.textedit)
 
         self.cmd_edit = QLineEdit()
         self.cmd_edit.editingFinished.connect(self.cmd_entr)
-
+        '''
+        outer_thread = QThread()
+        gui_runner.moveToThread(outer_thread)
+        '''
+        gui_runner.mysignal.connect(self.out_put_str)
+        main_box.addWidget(QLabel("DIALS command: "))
         main_box.addWidget(self.cmd_edit)
 
         self.main_widget = QWidget()
         self.main_widget.setLayout(main_box)
         self.setCentralWidget(self.main_widget)
+
+    def out_put_str(self, str_to_print):
+        self.textedit.moveCursor(QTextCursor.End)
+        str_w_new_lin = str_to_print + "\n"
+        self.textedit.insertPlainText(str_to_print)
+        app.processEvents()
 
     def cmd_entr(self):
         new_cmd = str(self.cmd_edit.text())
@@ -92,9 +105,9 @@ class MainWidget(QMainWindow):
         self.cmd_edit.setText("")
         self.cli_tree_output(self.uni_controler)
 
+        #TODO try to make this object/pickle compatible with C.L.I. app
         #with open('bkp.pickle', 'wb') as bkp_out:
         #    pickle.dump(self.uni_controler, bkp_out)
-
 
 
 if __name__ == '__main__':
