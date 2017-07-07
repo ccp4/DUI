b879746230e63cf52f4e99b89faa01fa23f58619
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2017-07-07 11:19:02 +0100

    removing commented code

--- lui_testing/mini_idials/m_idials_gui.py
+++ lui_testing/mini_idials/m_idials_gui.py
@@ -120,19 +120,12 @@ class MainWidget(QMainWindow):
         self.cli_tree_output(self.uni_controler)
 
         main_box = QVBoxLayout()
-        #top_hbox = QHBoxLayout()
 
-        ##################################################################
         h_main_splitter = QSplitter()
         h_main_splitter.setOrientation(Qt.Horizontal)
-        '''
-        h_main_splitter.addWidget( ...  )
-        '''
-        ##################################################################
 
         self.tree_out =TreeNavWidget()
         self.tree_out.clicked[QModelIndex].connect(self.item_clicked)
-        #top_hbox.addWidget(self.tree_out)
         h_main_splitter.addWidget(self.tree_out)
 
         self.cli_out = CliOutView(app = app)
@@ -144,10 +137,8 @@ class MainWidget(QMainWindow):
         self.my_tabs.addTab(self.cli_out, "CLI OutPut")
         self.my_tabs.addTab(self.web_view, "Report View")
 
-        #top_hbox.addWidget(self.my_tabs)
         h_main_splitter.addWidget(self.my_tabs)
 
-        #main_box.addLayout(top_hbox)
         main_box.addWidget(h_main_splitter)
 
         self.cmd_edit = QLineEdit()
