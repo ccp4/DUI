8894e6e966ebe971165d85d68ef5f0cefc237d72
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2017-09-07 15:17:09 +0100

    some needed lic comments

--- mini_idials_w_GUI/m_idials_gui.py
+++ mini_idials_w_GUI/m_idials_gui.py
@@ -1,8 +1,29 @@
+'''
+DUI's most central gidgets
+
+Author: Luis Fuentes-Montero (Luiso)
+With strong help from DIALS and CCP4 teams
+
+copyright (c) CCP4 - DLS
+'''
+
+#This program is free software; you can redistribute it and/or
+#modify it under the terms of the GNU General Public License
+#as published by the Free Software Foundation; either version 2
+#of the License, or (at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 from PyQt4.QtWebKit import *
-
-
 
 from outputs_n_viewers.web_page_view import WebTab
 from outputs_n_viewers.img_viewer import MyImgWin
@@ -18,8 +39,8 @@ import subprocess
 
 from custom_widgets import  ParamWidget
 
-widg_name_list = ["import", "find_spots", "index", "refine", "integrate"]
 
+widg_name_list = ["import", "find_spots", "index", "refine", "integrate"]
 
 def build_command_tip(command_lst):
     if(command_lst == [None]):
