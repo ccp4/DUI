dbc59878f5f2c0b236585f09b5be32d28b110501
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2017-09-21 14:34:07 +0100

    strating to work on bottom text/bar

--- mini_idials_w_GUI/gui_utils.py
+++ mini_idials_w_GUI/gui_utils.py
@@ -25,10 +25,12 @@ from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 from PyQt4.QtWebKit import *
 
+from time import sleep
+
 import sys
 
 class CliOutView(QTextEdit):
-    def __init__(self):
+    def __init__(self, app = None):
         super(CliOutView, self).__init__()
         self.setCurrentFont(QFont("Monospace"))
 
@@ -41,6 +43,28 @@ class CliOutView(QTextEdit):
         except:
             self.append(str_to_print[0])
 
+
+class Text_w_Bar(QProgressBar):
+    def __init__(self, parent = None):
+        super(Text_w_Bar,self).__init__()
+        self.setAlignment(Qt.AlignCenter)
+        self._text = ""
+
+    def setText(self, text):
+        self._text = text
+
+    def text(self):
+        return self._text
+
+    def start_motion(self):
+        print "starting motion"
+        self.setRange(0, 0)
+
+    def end_motion(self):
+        self.setRange(0, 1)
+        print "ending motion"
+
+
 class MainWidget(QMainWindow):
     def __init__(self):
         super(MainWidget, self).__init__()
@@ -49,13 +73,36 @@ class MainWidget(QMainWindow):
 
         self.tst_view = CliOutView(app = app)
         main_box.addWidget(self.tst_view)
+        self.txt_bar = Text_w_Bar()
+        main_box.addWidget(self.txt_bar)
+
+        btn1 = QPushButton(self)
+        btn1.clicked.connect(self.btn_1_clicked)
+        main_box.addWidget(btn1)
+
+        btn2 = QPushButton(self)
+        btn2.clicked.connect(self.btn_2_clicked)
+        main_box.addWidget(btn2)
+
+        self.n = 1
 
         self.main_widget = QWidget()
         self.main_widget.setLayout(main_box)
         self.setCentralWidget(self.main_widget)
+        self.show()
 
-        for n in xrange(5):
-            self.tst_view.add_txt("aaaaaaaaaaaaaaaaaaaa")
+    def btn_1_clicked(self):
+        self.txt_bar.start_motion()
+        self.n += 5
+        my_text = str(self.n) + "aaaaa bbbbbb a ccccccccc a" + str(self.n * self.n)
+        self.tst_view.add_txt(my_text)
+        self.txt_bar.setText(my_text)
+
+    def btn_2_clicked(self):
+        my_text = " Done "
+        self.tst_view.add_txt(my_text)
+        self.txt_bar.setText(my_text)
+        self.txt_bar.end_motion()
 
 if __name__ == '__main__':
     app =  QApplication(sys.argv)
