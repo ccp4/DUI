7f9f2c8b03ad0ae9ed2bbe82d11d1f7e22d22135
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2017-12-12 17:14:18 +0000

    moving some code in a more organized way

--- mini_idials_w_GUI/m_idials_gui.py
+++ mini_idials_w_GUI/m_idials_gui.py
@@ -29,109 +29,19 @@ import sys, os, psutil, pickle
 
 from outputs_n_viewers.web_page_view import WebTab
 from outputs_n_viewers.img_viewer import MyImgWin
-from cli_utils import TreeShow, get_next_step, prn_lst_lst_cmd
+from cli_utils import TreeShow, prn_lst_lst_cmd
 from m_idials import Runner
-from gui_utils import CliOutView, Text_w_Bar
+from gui_utils import CliOutView, Text_w_Bar, \
+     build_command_tip, update_info, update_pbar_msg
+
 from outputs_gui import InfoWidget
 from dynamic_reindex_gui import MyReindexOpts
 from custom_widgets import  ParamWidget
 
 
+
+
 widg_name_list = ["import", "find_spots", "index", "refine_bravais_settings", "refine", "integrate"]
-
-def build_command_tip(command_lst):
-    if(command_lst == [None]):
-        str_tip = "?"
-
-    else:
-        str_tip = "dials."+ str(command_lst[0])
-        for new_cmd in command_lst[1:]:
-            str_tip += "\n  " + str(new_cmd)
-
-    return str_tip
-
-def update_info(main_obj):
-    main_obj.cli_tree_output(main_obj.idials_runner)
-    new_html = main_obj.idials_runner.get_html_report()
-    new_img_json = main_obj.idials_runner.get_datablock_path()
-    new_ref_pikl = main_obj.idials_runner.get_reflections_path()
-
-    tmp_curr = main_obj.idials_runner.current_node
-    if(tmp_curr.success == None):
-        tmp_curr = tmp_curr.prev_step
-
-    uni_json = tmp_curr.json_file_out
-
-    print "\n new_html =", new_html , "\n"
-    print " new_img_json =", new_img_json , "\n"
-    print " new_ref_pikl =", new_ref_pikl , "\n"
-
-    if(main_obj.cur_html != new_html):
-        main_obj.cur_html = new_html
-        try:
-            main_obj.web_view.update_page(new_html)
-
-        except:
-            print "No HTML here"
-
-    if(main_obj.cur_pick != new_ref_pikl):
-        main_obj.cur_pick = new_ref_pikl
-        main_obj.img_view.ini_reflection_table(main_obj.cur_pick)
-
-    if(main_obj.cur_json != new_img_json):
-        main_obj.cur_json = new_img_json
-        main_obj.img_view.ini_datablock(main_obj.cur_json)
-        print "\n\n called .ini_datablock() \n\n"
-
-
-    main_obj.info_widget.update_data(exp_json_path = uni_json,
-                                     refl_pikl_path = new_ref_pikl)
-
-
-def update_pbar_msg(main_obj):
-    tmp_curr = main_obj.idials_runner.current_node
-    txt = str(tmp_curr.command_lst[0])
-
-    if(tmp_curr.success == False):
-        txt = "click << Re Try >> or navigate backwards in the tree"
-
-    elif(  (txt == "integrate"
-            or txt == "refine_bravais_settings")
-            and tmp_curr.success == True):
-
-        txt = "click << Re Try >> or navigate elsewhere in the tree"
-
-    elif(txt == "reindex" and tmp_curr.success == None):
-        txt = "click the blue row to run reindex"
-
-    elif(tmp_curr.success == None):
-        if(tmp_curr.lin_num == 1):
-            print "tmp_curr.lin_num == 1"
-            templ_text = main_obj.centre_widget.step_param_widg.currentWidget().my_widget.templ_lin.text()
-            print "templ_text =", templ_text
-            if(templ_text == " ? "):
-                txt = "click << Select File(s) >> or edit template line "
-
-            else:
-                txt = "click dials icon to run import"
-
-        else:
-            txt = "click dials icon to run " + txt
-
-    else:
-        nxt_cmd = get_next_step(tmp_curr)
-
-        if(nxt_cmd == None):
-            txt  ="Done"
-
-        else:
-            txt = "click <<" + nxt_cmd + ">> to go ahead, or click << Re Try >>"
-
-    main_obj.txt_bar.setText(txt)
-
-    print "\n\n update_pbar_msg =", txt
-    #print "get_next_step(tmp_curr) =", nxt_cmd, "\n\n"
-
 
 class MyThread(QThread):
 
