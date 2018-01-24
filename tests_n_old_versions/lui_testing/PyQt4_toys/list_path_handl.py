c6af9f079db54cc0891bb6b8e50d41c9e2076247
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2018-01-24 18:32:34 +0000

    working on the input page, playing with code example part 2

--- tests_n_old_versions/lui_testing/PyQt4_toys/list_path_handl.py
+++ tests_n_old_versions/lui_testing/PyQt4_toys/list_path_handl.py
@@ -1,5 +1,8 @@
 
 def get_run_str(in_str_lst):
+
+    print "in_str_lst =", in_str_lst
+
     selected_file_path = str(in_str_lst[0])
     fnd_sep = False
     sep_chr = None
@@ -19,62 +22,53 @@ def get_run_str(in_str_lst):
         return None
 
     dir_path = selected_file_path[:dir_pos_sep]
-    print "dir_path =", dir_path
 
     #TODO test if the next << if >> is actually needed
     if(dir_path[0:3] == "(u\'"):
         print "dir_path[0:3] == \"(u\'\""
         dir_path = dir_path[3:]
 
-    templ_str_tmp = selected_file_path[dir_pos_sep:]
+    templ_r_side = selected_file_path[dir_pos_sep:]
 
-    out_str = dir_path + templ_str_tmp
-    for pos, single_char in reversed(list(enumerate(templ_str_tmp))):
+    for pos, single_char in reversed(list(enumerate(templ_r_side))):
         if(single_char == "."):
             ext_pos_sep = pos
 
-    left_sd_name = templ_str_tmp[:ext_pos_sep]
-    ext_name = templ_str_tmp[ext_pos_sep:]
+    left_sd_name = templ_r_side[:ext_pos_sep]
+    ext_name = templ_r_side[ext_pos_sep:]
     if(ext_name == ".h5"):
         print "found h5 file"
-        out_str = left_sd_name
-        out_str = out_str + ext_name
+        file_name = left_sd_name
+        file_name = file_name + ext_name
         tail_size = 0
 
     else:
-        out_str = left_sd_name
+        file_name = left_sd_name
 
-        max_tail_size = int(len(templ_str_tmp) / 3)
+        max_tail_size = int(len(templ_r_side) / 3)
         for tail_size in xrange(max_tail_size):
-            prev_str = out_str
-            pos_to_replase = len(out_str) - tail_size - 1
+            prev_str = file_name
+            pos_to_replase = len(file_name) - tail_size - 1
             for num_char in '0123456789':
-                if out_str[pos_to_replase] == num_char:
-                    out_str = out_str[:pos_to_replase] + '#' + out_str[pos_to_replase + 1:]
+                if file_name[pos_to_replase] == num_char:
+                    file_name = file_name[:pos_to_replase] + '#' + file_name[pos_to_replase + 1:]
 
-            if(prev_str == out_str):
+            if(prev_str == file_name):
                 break
 
-        out_str = out_str + ext_name
-
-    templ_r_side= out_str
-    bak_pos = tail_size
-
-    print "templ_r_side, bak_pos =", templ_r_side, bak_pos
+        file_name = file_name + ext_name
 
     if(in_str_lst and len(in_str_lst) == 1):
-        out_str = dir_path + templ_r_side
+        out_str = dir_path + file_name
+        img_range = None
 
     else:
         str_lst = []
         for single_qstring in in_str_lst:
             str_lst.append(str(single_qstring))
 
-        print "\n str_lst =", str_lst, "\n"
-
         out_str = ""
         pos_last_num = 0
-        num_of_num = 0
 
         for pos in xrange(len(str_lst[0])):
             all_equal = True
@@ -102,11 +96,11 @@ def get_run_str(in_str_lst):
             try:
 
                 for single_string in str_lst:
-                        lst_num_str.append(int(single_string[pos_last_num-bak_pos:pos_last_num]))
+                    lst_num_str.append(int(single_string[pos_last_num-tail_size:
+                                                         pos_last_num]))
 
-                print "lst_num_str =", lst_num_str, "\n"
-                print "min(lst_num_str) = ", min(lst_num_str)
-                print "max(lst_num_str) = ", max(lst_num_str)
+                print "lst_num_str =", lst_num_str
+                img_range = [min(lst_num_str), max(lst_num_str)]
 
             except:
                 print "something went wrong with the range thing 01"
@@ -131,10 +125,14 @@ def get_run_str(in_str_lst):
         prev_char = single_char
 
     out_str = new_cmd
-    print "out_str( * mode ) =", out_str
+    print "img_range =", img_range
 
+    if(img_range != None):
+        out_str += " range=" + str(img_range[0]) + "," + str(img_range[1])
 
-    return dir_path, out_str
+    print "out_str( * mode ) =", out_str, "\n"
+
+    return out_str
 
 if(__name__ == "__main__"):
 
@@ -171,24 +169,12 @@ if(__name__ == "__main__"):
                     '/scratch/dui/dui_test/only_20_img_X4_wide/X4_wide_M1S4_2_0015.cbf']
 
 
-    dir_path, out_str = get_run_str(str_lst_ux)
-    print "dir_path", dir_path
-    print "out_str",  out_str, "\n\n\n"
-    dir_path, out_str = get_run_str(str_lst_wd)
-    print "dir_path", dir_path
-    print "out_str",  out_str, "\n\n\n"
-    dir_path, out_str = get_run_str(str_lst_single)
-    print "dir_path", dir_path
-    print "out_str",  out_str, "\n\n\n"
-    dir_path, out_str = get_run_str(str_lst_single_real)
-    print "dir_path", dir_path
-    print "out_str",  out_str, "\n\n\n"
-    dir_path, out_str = get_run_str(str_lst_real)
-    print "dir_path", dir_path
-    print "out_str",  out_str, "\n\n\n"
-    dir_path, out_str = get_run_str(str_lst_trick)
-    print "dir_path", dir_path
-    print "out_str",  out_str, "\n\n\n"
+    img_range = get_run_str(str_lst_ux)
+    img_range = get_run_str(str_lst_wd)
+    img_range = get_run_str(str_lst_single)
+    img_range = get_run_str(str_lst_single_real)
+    img_range = get_run_str(str_lst_real)
+    img_range = get_run_str(str_lst_trick)
 
 
 
