06da12a0cfef5eaf5e799cbcb0ed477ccacfed46
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2018-02-06 18:20:23 +0000

    adding example

--- tests_n_old_versions/lui_testing/PyQt4_toys/qprocess_pop_tst_01.py
+++ tests_n_old_versions/lui_testing/PyQt4_toys/qprocess_pop_tst_01.py
@@ -14,22 +14,10 @@ import sys
 class MyQProcess(QProcess):
     def __init__(self):
         super(MyQProcess, self).__init__()
-        #Create an instance variable here (of type QTextEdit)
-        self.edit  = QTextEdit()
-        self.edit.setWindowTitle("QTextEdit Standard Output Redirection")
-        self.edit.show()
+
         self.started.connect(self.on_start)
         self.readyReadStandardOutput.connect(self.readStdOutput)
         self.finished.connect(self.on_finish)
-
-
-        my_timer = QTimer()
-        my_timer.timeout.connect(self.on_timeout)
-
-    def readStdOutput(self):
-        line_string = str(self.readAllStandardOutput())
-        single_line = line_string[0:len(line_string) - 1]
-        self.edit.append(single_line)
 
     def on_start(self):
         print "on_start"
@@ -37,22 +25,23 @@ class MyQProcess(QProcess):
     def on_finish(self):
         print "on_finish"
 
-    def on_timeout(self):
-        print "on_timeout"
+    def readStdOutput(self):
+        line_string = str(self.readAllStandardOutput())
+        single_line = line_string[0:len(line_string) - 1]
+        print ">>", single_line
 
-def main():
+if __name__ == '__main__':
 
     app   = QApplication(sys.argv)
 
+    my_widg = QWidget()
+    my_widg.setWindowTitle("QTextEdit Standard Output Redirection")
+    my_widg.show()
+
     qProcess  = MyQProcess()
-    print "after MyQProcess.__init__()"
     qProcess.setProcessChannelMode(QProcess.MergedChannels);
-    print "after (setProcessChannelMode(QProcess.MergedChannels)"
     qProcess.start("./sec_interval.sh")
 
-    return app.exec_()
-
-if __name__ == '__main__':
-    main()
+    sys.exit(app.exec_())
 
 
