b9cdacea85e2112e40f0293fc5442e979c876e5b
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2018-05-07 21:26:56 +0100

    playing with reader part 2

--- tests_n_old_versions/lui_testing/python_mic/read_xia_logs_01.py
+++ tests_n_old_versions/lui_testing/python_mic/read_xia_logs_01.py
@@ -1,16 +1,22 @@
 import os
 
 def read_from_log(file_in):
-    print "\n Reading from file:", n_file
 
     with open(file_in) as myfile:
         all_lines = myfile.readlines()
 
     myfile.close()
+    print "\n\n             _____________________________________Reading from file:", n_file
 
-    for single_line in all_lines:
+    print "len(", file_in, ") =", len(all_lines)
+
+    max_len = len(all_lines)
+    if(max_len > 45):
+        max_len = 45
+
+    for single_line in all_lines[0:max_len]:
         print single_line,
-
+    print "\n\n"
 
 mini_path = "../../../../dui_test/X4_wide/xia2_run/DEFAULT/NATIVE/SWEEP1/"
 lst_dir = os.listdir(mini_path)
