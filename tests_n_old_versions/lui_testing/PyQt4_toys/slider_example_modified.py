6cf895e0be617f2f28fb31adbff959c3e7b8dd86
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2018-06-06 23:42:23 +0100

    adding slider example, thanks to Trolltech

---
+++ tests_n_old_versions/lui_testing/PyQt4_toys/slider_example_modified.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+from PyQt4 import QtCore, QtGui
+
+class SlidersGroup(QtGui.QGroupBox):
+
+    valueChanged = QtCore.pyqtSignal(int)
+
+    def __init__(self, title, parent=None):
+        super(SlidersGroup, self).__init__(title, parent)
+
+        self.slider = QtGui.QSlider(QtCore.Qt.Horizontal)
+        self.slider.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.slider.setTickPosition(QtGui.QSlider.TicksBothSides)
+        self.slider.setTickInterval(10)
+        self.slider.setSingleStep(1)
+
+        self.slider.valueChanged[int].connect(self.print_value)
+
+        direction = QtGui.QBoxLayout.TopToBottom
+
+        slidersLayout = QtGui.QBoxLayout(direction)
+        slidersLayout.addWidget(self.slider)
+        self.setLayout(slidersLayout)
+
+    def print_value(self, value):
+        print "value =", value
+
+    def setMinimum(self, value):
+        self.slider.setMinimum(value)
+
+    def setMaximum(self, value):
+        self.slider.setMaximum(value)
+
+    def invertAppearance(self, invert):
+        self.slider.setInvertedAppearance(invert)
+
+    def invertKeyBindings(self, invert):
+        self.slider.setInvertedControls(invert)
+
+class Window(QtGui.QWidget):
+    def __init__(self):
+        super(Window, self).__init__()
+
+        self.horizontalSliders = SlidersGroup("Horizontal")
+
+        layout = QtGui.QHBoxLayout()
+        layout.addWidget(self.horizontalSliders)
+        self.setLayout(layout)
+        self.setWindowTitle("Sliders")
+
+
+if __name__ == '__main__':
+
+    import sys
+
+    app = QtGui.QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
