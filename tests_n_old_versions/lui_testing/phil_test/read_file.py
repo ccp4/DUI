5b57b5a33676ec8958561da3a52b5ec3f3d31e0b
luisodls <luis.fuentes-montero@diamond.ac.uk>
2018-09-11 17:49:36 +0100

    adding example for testing and toying with phil files .....

--- tests_n_old_versions/lui_testing/phil_test/read_file.py
+++ tests_n_old_versions/lui_testing/phil_test/read_file.py
@@ -2,8 +2,8 @@ from dials.util.options import OptionParser
 
 def get_phil_par(path_to_file):
     print "path_to_file =", path_to_file
-    parser = OptionParser(phil = phil_scope)
+    parser = OptionParser(file_name=path_to_file)
     params, options = parser.parse_args(show_diff_phil=False)
 
 if(__name__ == "__main__"):
-    get_phil_par("/something/here")
+    get_phil_par("/scratch/dui/dui_test/only_20_img_X4_wide/dui_files/find_spots.phil")
