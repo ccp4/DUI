d35105921931f5430054f7ee9e0b4455063c56be
luisodls <luis.fuentes-montero@diamond.ac.uk>
2018-09-11 17:05:38 +0100

    adding example for testing and toying with phil files ...

--- tests_n_old_versions/lui_testing/phil_test/read_file.py
+++ tests_n_old_versions/lui_testing/phil_test/read_file.py
@@ -1,5 +1,9 @@
+from dials.util.options import OptionParser
+
 def get_phil_par(path_to_file):
     print "path_to_file =", path_to_file
+    parser = OptionParser(phil = phil_scope)
+    params, options = parser.parse_args(show_diff_phil=False)
 
 if(__name__ == "__main__"):
     get_phil_par("/something/here")
