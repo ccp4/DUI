e37d16826449d100ecedbaef3d75f4415f60b978
luisodls <luis.fuentes-montero@diamond.ac.uk>
2018-09-12 15:44:26 +0100

    testing phil object usage

--- tests_n_old_versions/lui_testing/phil_test/read_file.py
+++ tests_n_old_versions/lui_testing/phil_test/read_file.py
@@ -1,9 +1,19 @@
-from dials.util.options import OptionParser
+'''
+libtbx.phil.parse(
+    file_name=file_name,
+    converter_registry=None,
+    process_includes=co.process_includes)
+'''
 
+from dials.util.options import OptionParser
+import libtbx.load_env
+import libtbx.phil
 def get_phil_par(path_to_file):
+
     print "path_to_file =", path_to_file
-    parser = OptionParser(file_name=path_to_file)
-    params, options = parser.parse_args(show_diff_phil=False)
+    obj = libtbx.phil.parse(file_name=path_to_file)
+    print "type(obj) =", type(obj)
+    print "obj =", obj
 
 if(__name__ == "__main__"):
     get_phil_par("/scratch/dui/dui_test/only_20_img_X4_wide/dui_files/find_spots.phil")
