fec1e560786795c23aaa62ba8c81839739e5a193
Nicholas Devenish <ndevenish@gmail.com>
2019-01-08 18:05:14 +0000

    Make creating a new DUI instance in tests simpler

--- tests/test_basic_styles.py
+++ tests/test_basic_styles.py
@@ -6,10 +6,16 @@ from dui.m_idials_gui import MainWidget
 from dui.cli_utils import sys_arg
 
 
-@pytest.mark.gui
-def test_basic_window(qtbot, screenshots, tmpdir):
+@pytest.fixture
+def new_dui(qtbot, tmpdir):
+    """Fixture to give a new, empty DUI MainWindow Widget."""
     sys_arg.directory = str(tmpdir)
     win = MainWidget()
     win.show()
     win.resize(1024, 768)
-    screenshots.saveWidget(win)
+    return win
+
+
+@pytest.mark.gui
+def test_basic_window(new_dui, screenshots):
+    screenshots.saveWidget(new_dui)
