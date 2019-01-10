ccp4/DUI

commit 95e4323932d9c0f506e15f736e22eeaea87e0e58

Display a message when loading errors happen
This means DUI can't run, but is better than silently deleting all of
their work - either they've made a mistake, or it might be recoverable,
but in any case it'll never be fixed if we don't get a chance of 
being reported the message.

src/dui/m_idials_gui.py
@@ -25,7 +25,9 @@ from __future__ import absolute_import, division, print_function
 import logging
 import os
 import pickle
-import shutil
+import traceback
+
+from six import raise_from
 
 from .dynamic_reindex_gui import MyReindexOpts
 from .cli_utils import TreeShow, prn_lst_lst_cmd, sys_arg
@@ -289,33 +291,41 @@ class StopRunRetry(QWidget):
         # self.show()
 
 
+class DUIDataLoadingError(Exception):
+    def __init__(self, original):
+        self.original_traceback = original
+
+
+def load_previous_state(dui_files_path):
+    with open(os.path.join(dui_files_path, "dui_files/bkp.pickle"), "rb") as bkp_in:
+        return pickle.load(bkp_in)
+
+
 class MainWidget(QMainWindow):
     def __init__(self):
         super(MainWidget, self).__init__()
+
+        self.my_pop = None  # Any child popup windows. Only bravais_table ATM
         self.storage_path = sys_arg.directory
-        try:
-            with open(self.storage_path + "/dui_files/bkp.pickle", "rb") as bkp_in:
-                self.idials_runner = pickle.load(bkp_in)
 
-            # TODO sometimes the following error appears
-            # Attribute not found
-            #   'module' object has no attribute 'CommandNode'
+        refresh_gui = False
 
-            refresh_gui = True
-
-        except Exception as e:
-            logger.debug("str(e) = %s", str(e))
-            logger.debug("e.__doc__ = %s", e.__doc__)
-            logger.debug("e.message = %s", e.message)
-            self.idials_runner = Runner()
-
+        # Load the previous state of DUI, if present
+        dui_files_path = os.path.join(self.storage_path, "dui_files")
+        if os.path.isdir(dui_files_path):
             try:
-                shutil.rmtree(self.storage_path + "/dui_files")
-            except OSError:
-                logger.debug('failed to do "shutil.rmtree("/dui_files")"')
-
-            os.mkdir(self.storage_path + "/dui_files")
-            refresh_gui = False
+                self.idials_runner = load_previous_state(dui_files_path)
+                raise RuntimeError("Something something error")
+            except Exception as e:
+                # Something went wrong - tell the user then close
+                msg = traceback.format_exc()
+                logger.error("ERROR LOADING PREVIOUS DATA:\n%s", msg)
+                raise_from(DUIDataLoadingError(msg), e)
+            refresh_gui = True
+        else:
+            # No dui_files path - start with a fresh state
+            os.mkdir(dui_files_path)
+            self.idials_runner = Runner()
 
         self.cli_tree_output = TreeShow()
         self.cli_tree_output(self.idials_runner)

tests/test_basic_styles.py
@@ -1,9 +1,12 @@
 # coding: utf-8
 
+"""Test running DUI, but in a screenshot-friendly way"""
+
 import pytest
 
-from dui.m_idials_gui import MainWidget
+from dui.m_idials_gui import MainWidget, DUIDataLoadingError
 from dui.cli_utils import sys_arg
+from dui.gui_utils import loading_error_dialog
 
 
 @pytest.fixture
@@ -19,3 +22,15 @@ def new_dui(qtbot, tmpdir):
 @pytest.mark.gui
 def test_basic_window(new_dui, screenshots):
     screenshots.saveWidget(new_dui)
+
+
+def test_error_dialog(screenshots, tmpdir):
+    sys_arg.directory = str(tmpdir)
+    # Ensure path is created so DUI tries to load
+    (tmpdir / "dui_files").mkdir()
+    with pytest.raises(DUIDataLoadingError) as excinfo:
+        MainWidget()
+    # Load and show the error dialog for a screenshot
+    dialog = loading_error_dialog(excinfo.value.original_traceback)
+    dialog.show()
+    screenshots.saveWidget(dialog)

src/dui/gui_utils.py
@@ -28,22 +28,18 @@ import logging
 import os
 import re
 import subprocess
-import sys
 
 import psutil
 
 from .cli_utils import get_next_step, sys_arg, get_phil_par
 from .qt import (
-    QApplication,
     QDialog,
     QFont,
     QHeaderView,
     QIcon,
     QLabel,
-    QMainWindow,
     QProgressBar,
     QPushButton,
-    QToolButton,
     QSize,
     QSizePolicy,
     QStandardItem,
@@ -53,10 +49,12 @@ from .qt import (
     QT5,
     QTextEdit,
     QThread,
+    QToolButton,
     QTreeView,
     QVBoxLayout,
     QWidget,
     Signal,
+    uic,
 )
 from six.moves import range
 
@@ -194,6 +192,11 @@ def kill_w_child(pid_num):
 def get_main_path():
     """Get the path of the root of the DUI package"""
     return str(os.path.dirname(os.path.abspath(__file__)))
+
+
+def get_package_path(path):
+    """Get the absolute path of something under the DUI package"""
+    return os.path.join(get_main_path(), path)
 
 
 def get_import_run_string(in_str_lst):
@@ -807,63 +810,9 @@ class Text_w_Bar(QProgressBar):
         logger.debug("ending motion")
 
 
-class MainWidget(QMainWindow):
-    """
-    This is a test GUI only used by the developer
-    the user should NEVER see this code running
-    """
-
-    def __init__(self):
-        super(MainWidget, self).__init__()
-        main_box = QVBoxLayout()
-        main_box.addWidget(QLabel("Test dummy GUI"))
-
-        self.tst_view = CliOutView(app=app)
-        main_box.addWidget(self.tst_view)
-        self.txt_bar = Text_w_Bar()
-        main_box.addWidget(self.txt_bar)
-
-        btn1 = QPushButton("\n Do \n", self)
-        btn1.clicked.connect(self.btn_1_clicked)
-        main_box.addWidget(btn1)
-
-        btn2 = QPushButton("\n Stop \n", self)
-        btn2.clicked.connect(self.btn_2_clicked)
-        main_box.addWidget(btn2)
-
-        btn3 = QPushButton("\n refresh text \n", self)
-        btn3.clicked.connect(self.btn_3_clicked)
-        main_box.addWidget(btn3)
-
-        self.n = 1
-
-        self.main_widget = QWidget()
-        self.main_widget.setLayout(main_box)
-        self.setCentralWidget(self.main_widget)
-        # self.show()
-
-    def btn_1_clicked(self):
-        self.txt_bar.start_motion()
-        self.n += 5
-        my_text = str(self.n) + "aaaaa bbbbbb a ccccccccc a" + str(self.n * self.n)
-        self.tst_view.add_txt(my_text)
-        self.txt_bar.setText(my_text)
-
-    def btn_2_clicked(self):
-        my_text = " Done "
-        self.tst_view.add_txt(my_text)
-        self.txt_bar.setText(my_text)
-        self.txt_bar.end_motion()
-
-    def btn_3_clicked(self):
-        # TODO update this path
-        self.tst_view.refresh_txt(
-            "../../dui_test/X4_wide/reuse_area/dials_files/2_find_spots.log"
-        )
-
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    ex = MainWidget()
-    ex.show()
-    sys.exit(app.exec_())
+def loading_error_dialog(message):
+    """Create an error message about loading in a dialog box."""
+    dialog_filename = get_package_path("resources/error_loading_dialog.ui")
+    dialog = uic.loadUi(dialog_filename)
+    dialog.errorMessage.setPlainText(message)
+    return dialog

src/dui/main_dui.py
@@ -57,6 +57,10 @@ def main():
             level=logging.DEBUG, format="%(levelname)s:%(name)s:%(lineno)s %(message)s"
         )
 
+    # Turn things off unless at varying depths of verbosity
+    if args.verbose <= 2:
+        logging.getLogger("PyQt4.uic").setLevel(logging.WARNING)
+
     # Process the phil-style parameters
     for arg in args.positionals[:]:
         if arg.startswith("template="):
@@ -80,15 +84,25 @@ def main():
 
     # Inline import so that we can load this after logging setup
     from .qt import QApplication, QStyleFactory
-    from .m_idials_gui import MainWidget
+    from .m_idials_gui import MainWidget, DUIDataLoadingError
+    from .gui_utils import loading_error_dialog
 
     app = QApplication(sys.argv)
     logger.debug(
         "QT Style: %s [%s]", app.style().objectName(), ", ".join(QStyleFactory.keys())
     )
 
-    ex = MainWidget()
+    try:
+        ex = MainWidget()
+    except DUIDataLoadingError as e:
+        ex = loading_error_dialog(e.original_traceback)
+
     ex.show()
+
+    # Needed for a QT4 bug(?) on mac - windows don't steal focus on open
+    ex.activateWindow()
+    ex.raise_()
+
     sys.exit(app.exec_())
 
 

src/dui/qt.py
@@ -19,6 +19,7 @@ try:
     from PyQt5.QtGui import *
     from PyQt5.QtWebKit import *
     from PyQt5.QtWebKitWidgets import *
+    from PyQt5 import uic
 
     # Signal implementation changes slightly across implementations
     Signal = pyqtSignal
@@ -51,6 +52,7 @@ except ImportError:
         from PyQt4.QtGui import *
         from PyQt4.QtCore import *
         from PyQt4.QtWebKit import *
+        from PyQt4 import uic
 
         Signal = pyqtSignal
 
@@ -65,6 +67,7 @@ except ImportError:
         from PySide.QtGui import *
         from PySide.QtCore import *
         from PySide.QtWebKit import *
+        from PySide import uic
 
         QT5 = False
         # In case we're using pytest-qt, force the same API

src/dui/resources/error_loading_dialog.ui
@@ -1,0 +1,82 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>LoadErrorDialog</class>
+ <widget class="QDialog" name="LoadErrorDialog">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>597</width>
+    <height>292</height>
+   </rect>
+  </property>
+  <property name="windowTitle">
+   <string>Error loading DUI</string>
+  </property>
+  <property name="modal">
+   <bool>false</bool>
+  </property>
+  <layout class="QGridLayout" name="gridLayout">
+   <item row="0" column="0">
+    <layout class="QVBoxLayout" name="verticalLayout">
+     <item>
+      <widget class="QLabel" name="label">
+       <property name="text">
+        <string>An unexpected error occured whilst loading your previous DUI workspace:</string>
+       </property>
+      </widget>
+     </item>
+     <item>
+      <widget class="QPlainTextEdit" name="errorMessage">
+       <property name="lineWrapMode">
+        <enum>QPlainTextEdit::NoWrap</enum>
+       </property>
+       <property name="readOnly">
+        <bool>true</bool>
+       </property>
+      </widget>
+     </item>
+     <item>
+      <widget class="QLabel" name="label_2">
+       <property name="text">
+        <string>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Please report this error to &lt;a href=&quot;mailto:dials-support@lists.sourceforge.net&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;dials-support@lists.sourceforge.net&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;Please move or remove the &lt;tt&gt;dui-files/&lt;/tt&gt; subfolder or change your working directory to continue.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</string>
+       </property>
+       <property name="openExternalLinks">
+        <bool>true</bool>
+       </property>
+      </widget>
+     </item>
+     <item>
+      <widget class="QDialogButtonBox" name="buttonBox">
+       <property name="orientation">
+        <enum>Qt::Horizontal</enum>
+       </property>
+       <property name="standardButtons">
+        <set>QDialogButtonBox::Close</set>
+       </property>
+      </widget>
+     </item>
+    </layout>
+   </item>
+  </layout>
+ </widget>
+ <resources/>
+ <connections>
+  <connection>
+   <sender>buttonBox</sender>
+   <signal>rejected()</signal>
+   <receiver>LoadErrorDialog</receiver>
+   <slot>close()</slot>
+   <hints>
+    <hint type="sourcelabel">
+     <x>316</x>
+     <y>260</y>
+    </hint>
+    <hint type="destinationlabel">
+     <x>298</x>
+     <y>145</y>
+    </hint>
+   </hints>
+  </connection>
+ </connections>
+</ui>

commit 1ecdaea63f71ab7f92f08a62afc765e7ca43aaf5

Fix errors loading empty/unsaved dui_files folders
Minor problem introduced in 95e4323932d9c0f506e15f736e22eeaea87e0e58
where a folder would be created empty and this would cause a loading
error the next time. Added a test for this. Now, missing pickle files
are treated as though the folder is clean - this *might* cause data
problems but seems the cleanest way for now.

Also, left some debug statement in that meant that previous workspaces
weren't reloaded successfully. That this works currently doesn't have a
test.

src/dui/m_idials_gui.py
@@ -297,7 +297,7 @@ class DUIDataLoadingError(Exception):
 
 
 def load_previous_state(dui_files_path):
-    with open(os.path.join(dui_files_path, "dui_files/bkp.pickle"), "rb") as bkp_in:
+    with open(os.path.join(dui_files_path, "bkp.pickle"), "rb") as bkp_in:
         return pickle.load(bkp_in)
 
 
@@ -312,10 +312,9 @@ class MainWidget(QMainWindow):
 
         # Load the previous state of DUI, if present
         dui_files_path = os.path.join(self.storage_path, "dui_files")
-        if os.path.isdir(dui_files_path):
+        if os.path.isfile(os.path.join(dui_files_path, "bkp.pickle")):
             try:
                 self.idials_runner = load_previous_state(dui_files_path)
-                raise RuntimeError("Something something error")
             except Exception as e:
                 # Something went wrong - tell the user then close
                 msg = traceback.format_exc()
@@ -324,7 +323,8 @@ class MainWidget(QMainWindow):
             refresh_gui = True
         else:
             # No dui_files path - start with a fresh state
-            os.mkdir(dui_files_path)
+            if not os.path.isdir(dui_files_path):
+                os.mkdir(dui_files_path)
             self.idials_runner = Runner()
 
         self.cli_tree_output = TreeShow()

tests/test_basic_functions.py
@@ -1,0 +1,16 @@
+# coding: utf-8
+
+"""Test running DUI, but in a screenshot-friendly way"""
+
+import pytest
+
+from dui.m_idials_gui import MainWidget
+from dui.cli_utils import sys_arg
+
+
+@pytest.mark.gui
+def test_reopen_without_write(qtbot, tmpdir):
+    sys_arg.directory = str(tmpdir)
+    # Open twice.
+    MainWidget().close()
+    MainWidget().close()

tests/test_basic_styles.py
@@ -28,6 +28,7 @@ def test_error_dialog(screenshots, tmpdir):
     sys_arg.directory = str(tmpdir)
     # Ensure path is created so DUI tries to load
     (tmpdir / "dui_files").mkdir()
+    (tmpdir / "dui_files" / "bkp.pickle").open("w").close()
     with pytest.raises(DUIDataLoadingError) as excinfo:
         MainWidget()
     # Load and show the error dialog for a screenshot