3a966145b4a7fb81efd89eb614137ffb519b09f6
Nicholas Devenish <ndevenish@gmail.com>
2020-09-01 22:24:06 +0100

    Add a UIC-compiling setup.py command 'build_qt'

--- setup.py
+++ setup.py
@@ -1,12 +1,55 @@
 import os
+import subprocess
+from glob import glob
 
-from setuptools import setup
+from setuptools import Command, setup
 
 # Read the DUI version out of a version file in the package
 _ver_file = os.path.join(os.path.dirname(__file__), "src", "dui", "_version.py")
 _ver_locals = {}
 exec(open(_ver_file).read(), _ver_locals)
 DUI_VERSION = _ver_locals["__version__"]
+
+# Borrowed from: https://github.com/glue-viz/glue/blob/master/setup.py
+# Copyright (c) 2013, Glue developers
+
+
+# Based off https://gist.github.com/ivanalejandro0/6758741
+class BuildQt(Command):
+    """
+    Defines a command for setup.py that compiles the *.ui and *.qrc files
+    into python files.
+    It looks for *.ui files in _UI_PATH subfolder.
+    """
+
+    _UI_PATH = os.path.join("src", "dui", "resources")
+
+    user_options = [("uic=", "u", "Custom uic command (usually pyside-uic or pyuic4)")]
+
+    def initialize_options(self):
+        """
+        Sets the proper command names for the compiling tools.
+        """
+        self.pyuic = "pyside2-uic"
+
+    def finalize_options(self):
+        pass
+
+    def _compile_ui(self, infile, outfile):
+        try:
+            subprocess.call([self.pyuic, infile, "-o", outfile])
+        except OSError:
+            print("uic command failed - make sure that pyside-uic " "is in your $PATH")
+
+    def run(self):
+        # compile ui files
+        for infile in glob(os.path.join(self._UI_PATH, "*.ui")):
+            directory, ui_filename = os.path.split(infile)
+            py_filename = ui_filename.replace(".ui", ".py")
+            outfile = os.path.join(directory, "ui_" + py_filename)
+            print("Compiling: {0} -> {1}".format(infile, outfile))
+            self._compile_ui(infile, outfile)
+
 
 setup(
     name="dui",
@@ -63,6 +106,7 @@ setup(
     ],
     include_package_data=True,
     entry_points={"console_scripts": ["dui=dui.main_dui:main"]},
+    cmdclass={"build_qt": BuildQt},
 )
 
 # TODO(nick): Work out how to get requirements working, including non-pip like PyQT
